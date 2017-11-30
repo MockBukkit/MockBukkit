c111d5b167dfc234706548db69365f1056c55788
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2017-11-30 22:21:05 +0100

    Add method to access ServerMock

--- src/main/java/be/seeseemelk/mockbukkit/MockBukkit.java
+++ src/main/java/be/seeseemelk/mockbukkit/MockBukkit.java
@@ -4,12 +4,30 @@ import org.bukkit.Bukkit;
 
 public class MockBukkit
 {
+	private static ServerMock mock = null;
+	
 	/**
 	 * Start mocking the <code>Bukkit</code> singleton.
 	 */
 	public static void mock()
 	{
-		ServerMock mock = new ServerMock();
-		Bukkit.setServer(mock);
+		if (mock == null)
+		{
+			mock = new ServerMock();
+			Bukkit.setServer(mock);
+		}
+		else
+		{
+			throw new UnsupportedOperationException("Already mocking");
+		}
+	}
+	
+	/**
+	 * Get the mock server instance.
+	 * @return The {@link ServerMock} instance or {@code null} if none is set up yet.
+	 */
+	public static ServerMock getMock()
+	{
+		return mock;
 	}
 }
