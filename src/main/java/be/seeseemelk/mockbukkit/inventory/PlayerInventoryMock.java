f2da1e79f19f06c3d01c738b6eb69b533c9b629d
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2017-12-11 14:36:41 +0100

    Add test cases for PlayerInventoryMock

--- src/main/java/be/seeseemelk/mockbukkit/inventory/PlayerInventoryMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/inventory/PlayerInventoryMock.java
@@ -35,34 +35,33 @@ public class PlayerInventoryMock implements PlayerInventory
 	{
 		return 40;
 	}
-
-	@Override
-	public int getMaxStackSize()
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public void setMaxStackSize(int size)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-
-	}
-
+	
 	@Override
 	public String getName()
 	{
 		return name;
 	}
-
+	
 	@Override
 	public ItemStack getItem(int index)
 	{
 		return items[index];
 	}
 	
+	@Override
+	public void setItem(int index, ItemStack item)
+	{
+		items[index] = item;
+	}
+
+	/**
+	 * Adds a single item to the inventory. Returns whatever item it couldn't add.
+	 * 
+	 * @param item
+	 *            The item to add.
+	 * @return The remaining stack that couldn't be added. If it's empty it just
+	 *         returns {@code null}.
+	 */
 	public ItemStack addItem(ItemStack item)
 	{
 		for (int i = 0; i < items.length; i++)
@@ -90,7 +89,7 @@ public class PlayerInventoryMock implements PlayerInventory
 		
 		return item;
 	}
-
+	
 	@Override
 	public HashMap<Integer, ItemStack> addItem(ItemStack... items) throws IllegalArgumentException
 	{
@@ -106,20 +105,13 @@ public class PlayerInventoryMock implements PlayerInventory
 		}
 		return notSaved;
 	}
-
-	@Override
-	public HashMap<Integer, ItemStack> removeItem(ItemStack... items) throws IllegalArgumentException
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
+	
 	@Override
 	public ItemStack[] getContents()
 	{
 		return items;
 	}
-
+	
 	@Override
 	public void setContents(ItemStack[] items) throws IllegalArgumentException
 	{
@@ -135,372 +127,386 @@ public class PlayerInventoryMock implements PlayerInventory
 			}
 		}
 	}
-
+	
+	@Override
+	public int getMaxStackSize()
+	{
+		// TODO Auto-generated method stub
+		throw new UnimplementedOperationException();
+	}
+	
+	@Override
+	public void setMaxStackSize(int size)
+	{
+		// TODO Auto-generated method stub
+		throw new UnimplementedOperationException();
+		
+	}
+	
+	@Override
+	public HashMap<Integer, ItemStack> removeItem(ItemStack... items) throws IllegalArgumentException
+	{
+		// TODO Auto-generated method stub
+		throw new UnimplementedOperationException();
+	}
+	
 	@Override
 	public ItemStack[] getStorageContents()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public void setStorageContents(ItemStack[] items) throws IllegalArgumentException
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public boolean contains(int materialId)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public boolean contains(Material material) throws IllegalArgumentException
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public boolean contains(ItemStack item)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public boolean contains(int materialId, int amount)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public boolean contains(Material material, int amount) throws IllegalArgumentException
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public boolean contains(ItemStack item, int amount)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public boolean containsAtLeast(ItemStack item, int amount)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public HashMap<Integer, ? extends ItemStack> all(int materialId)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public HashMap<Integer, ? extends ItemStack> all(Material material) throws IllegalArgumentException
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public HashMap<Integer, ? extends ItemStack> all(ItemStack item)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public int first(int materialId)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public int first(Material material) throws IllegalArgumentException
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public int first(ItemStack item)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public int firstEmpty()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public void remove(int materialId)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void remove(Material material) throws IllegalArgumentException
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void remove(ItemStack item)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void clear(int index)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void clear()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public List<HumanEntity> getViewers()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public String getTitle()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public InventoryType getType()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ListIterator<ItemStack> iterator()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ListIterator<ItemStack> iterator(int index)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public Location getLocation()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ItemStack[] getArmorContents()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ItemStack[] getExtraContents()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ItemStack getHelmet()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ItemStack getChestplate()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ItemStack getLeggings()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public ItemStack getBoots()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
-	@Override
-	public void setItem(int index, ItemStack item)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-
-	}
-
+	
 	@Override
 	public void setArmorContents(ItemStack[] items)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void setExtraContents(ItemStack[] items)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void setHelmet(ItemStack helmet)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void setChestplate(ItemStack chestplate)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void setLeggings(ItemStack leggings)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public void setBoots(ItemStack boots)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public ItemStack getItemInMainHand()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public void setItemInMainHand(ItemStack item)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public ItemStack getItemInOffHand()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public void setItemInOffHand(ItemStack item)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public ItemStack getItemInHand()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public void setItemInHand(ItemStack stack)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public int getHeldItemSlot()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public void setHeldItemSlot(int slot)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
-
+		
 	}
-
+	
 	@Override
 	public int clear(int id, int data)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 	@Override
 	public HumanEntity getHolder()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
-
+	
 }
---
+++ src/test/java/be/seeseemelk/mockbukkit/inventory/PlayerInventoryMockTest.java
@@ -0,0 +1,124 @@
+package be.seeseemelk.mockbukkit.inventory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+
+public class PlayerInventoryMockTest
+{
+	private PlayerInventoryMock inventory;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		MockBukkit.mock();
+		inventory = new PlayerInventoryMock("Inventory");
+	}
+	
+	@After
+	public void tearDown() throws Exception
+	{
+		MockBukkit.unload();
+	}
+	
+	@Test
+	public void getName_Default_CorrectName()
+	{
+		assertEquals("Inventory", inventory.getName());
+	}
+	
+	@Test
+	public void getSize_Default_40()
+	{
+		assertEquals(40, inventory.getSize());
+	}
+	
+	@Test
+	public void getItem_Default_AllNull()
+	{
+		for (int i = 0; i < inventory.getSize(); i++)
+		{
+			assertNull(inventory.getItem(i));
+		}
+	}
+	
+	@Test
+	public void addItem_EmptyInventoryAddsOneStack_OneStackUsed()
+	{
+		ItemStack stack = new ItemStack(Material.DIRT, 64);
+		ItemStack remaining = inventory.addItem(stack);
+		assertNull(remaining);
+		ItemStack stored = inventory.getItem(0);
+		assertEquals(stored.getAmount(), 64);
+		ItemStack next = inventory.getItem(1);
+		assertNull(next);
+	}
+	
+	@Test
+	public void addItem_FullInventoryAddsOneStack_NothingAdded()
+	{
+		ItemStack filler = new ItemStack(Material.COBBLESTONE, 1);
+		for (int i = 0; i < inventory.getSize(); i++)
+		{
+			inventory.setItem(i, filler);
+		}
+		
+		ItemStack stack = new ItemStack(Material.DIRT, 64);
+		ItemStack remaining = inventory.addItem(stack);
+		assertEquals(64, remaining.getAmount());
+		
+		for (ItemStack stored : inventory.getContents())
+		{
+			assertEquals(1, stored.getAmount());
+			assertEquals(Material.COBBLESTONE, stored.getType());
+		}
+	}
+	
+	@Test
+	public void addItem_PartiallyFilled_AddsOneStack_HalfAdded()
+	{
+		ItemStack filler = new ItemStack(Material.COBBLESTONE, 1);
+		for (int i = 2; i < inventory.getSize(); i++)
+		{
+			inventory.setItem(i, filler);
+		}
+		ItemStack preset = new ItemStack(Material.DIRT, 48);
+		inventory.setItem(0, preset);
+		inventory.setItem(1, preset);
+		
+		ItemStack store = new ItemStack(Material.DIRT, 64);
+		ItemStack remaining = inventory.addItem(store);
+		assertEquals(32, remaining.getAmount());
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
