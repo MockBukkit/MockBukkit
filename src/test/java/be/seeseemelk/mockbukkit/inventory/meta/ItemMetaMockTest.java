23765b539436f8a4d544ac69cb3c9142d7bcc8ff
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2018-02-05 08:07:38 +0100

    Remove unused import

--- src/test/java/be/seeseemelk/mockbukkit/inventory/meta/ItemMetaMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/inventory/meta/ItemMetaMockTest.java
@@ -2,7 +2,6 @@ package be.seeseemelk.mockbukkit.inventory.meta;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
