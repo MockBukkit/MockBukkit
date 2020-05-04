e60f0ecb816b86397082c1439345516ccfe332da
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-05-04 12:51:34 +0200

    Added EnchantedBookMetaMock Unit Test

---
+++ src/test/java/be/seeseemelk/mockbukkit/inventory/meta/EnchantmentMock.java
@@ -0,0 +1,67 @@
+package be.seeseemelk.mockbukkit.inventory.meta;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This mocks the implementation of the {@link Enchantment} class.
+ * Tests would fail otherwise since the constants found in that class only work during
+ * the server's runtime.
+ * 
+ * @author TheBusyBiscuit
+ *
+ */
+public class EnchantmentMock extends Enchantment {
+
+    private final String name;
+
+    public EnchantmentMock(@NotNull String name) {
+        super(NamespacedKey.randomKey());
+        this.name = name;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 3;
+    }
+
+    @Override
+    public @NotNull EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.ALL;
+    }
+
+    @Override
+    public boolean canEnchantItem(@NotNull ItemStack item) {
+        return true;
+    }
+
+    @Override
+    public boolean conflictsWith(@NotNull Enchantment other) {
+        // We just let it only conflict with itself, it's enough for our tests
+        return other == this;
+    }
+
+}
