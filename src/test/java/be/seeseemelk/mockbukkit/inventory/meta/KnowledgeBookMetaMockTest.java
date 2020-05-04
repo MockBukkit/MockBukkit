592c62ab625f5760df9326dbafca694092836388
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-05-04 12:51:46 +0200

    Added KnowledgeBookMetaMock Unit Test

---
+++ src/test/java/be/seeseemelk/mockbukkit/inventory/meta/KnowledgeBookMetaMockTest.java
@@ -0,0 +1,105 @@
+package be.seeseemelk.mockbukkit.inventory.meta;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.NamespacedKey;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+
+public class KnowledgeBookMetaMockTest {
+    
+    private final int MAX_RECIPES = 32767;
+
+    @Before
+    public void setUp() {
+        ServerMock mock = MockBukkit.mock();
+    }
+
+    @After
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    public void hasRecipesDefaultFalse() {
+        KnowledgeBookMetaMock meta = new KnowledgeBookMetaMock();
+        assertFalse(meta.hasRecipes());
+    }
+
+    @Test
+    public void addRecipe() {
+        KnowledgeBookMetaMock meta = new KnowledgeBookMetaMock();
+        NamespacedKey key = NamespacedKey.randomKey();
+
+        assertFalse(meta.hasRecipes());
+        meta.addRecipe(key);
+        assertTrue(meta.hasRecipes());
+    }
+
+    @Test
+    public void addNullRecipeAndFail() {
+        KnowledgeBookMetaMock meta = new KnowledgeBookMetaMock();
+        List<NamespacedKey> recipes = Arrays.asList(null, null, null);
+
+        assertFalse(meta.hasRecipes());
+        meta.setRecipes(recipes);
+        assertFalse(meta.hasRecipes());
+    }
+
+    @Test
+    public void setRecipes() {
+        KnowledgeBookMetaMock meta = new KnowledgeBookMetaMock();
+        List<NamespacedKey> recipes = Arrays.asList(NamespacedKey.randomKey(), NamespacedKey.randomKey());
+
+        assertFalse(meta.hasRecipes());
+        meta.setRecipes(recipes);
+        assertTrue(meta.hasRecipes());
+    }
+
+    @Test
+    public void getRecipes() {
+        KnowledgeBookMetaMock meta = new KnowledgeBookMetaMock();
+        List<NamespacedKey> recipes = Arrays.asList(NamespacedKey.randomKey(), NamespacedKey.randomKey());
+        meta.setRecipes(recipes);
+
+        assertEquals(recipes, meta.getRecipes());
+    }
+
+    @Test
+    public void addTooManyRecipes() {
+        KnowledgeBookMetaMock meta = new KnowledgeBookMetaMock();
+        
+        for (int i = 0; i < MAX_RECIPES + 50; i++) {
+            meta.addRecipe(NamespacedKey.randomKey());
+        }
+        
+        assertEquals(MAX_RECIPES, meta.getRecipes().size());
+    }
+
+    @Test
+    public void equalsEnchantmentMeta() {
+        KnowledgeBookMetaMock meta = new KnowledgeBookMetaMock();
+        assertEquals(meta, meta);
+        assertFalse(meta.equals(new ItemMetaMock()));
+
+        KnowledgeBookMetaMock meta2 = new KnowledgeBookMetaMock();
+        assertEquals(meta, meta2);
+        
+        NamespacedKey recipe = NamespacedKey.randomKey();
+
+        meta.addRecipe(recipe);
+        assertFalse(meta.equals(meta2));
+
+        meta2.addRecipe(recipe);
+        assertEquals(meta, meta2);
+    }
+}
