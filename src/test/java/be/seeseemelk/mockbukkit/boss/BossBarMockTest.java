c57d666aecd922c3de42615e47ba5520e99d3712
o <ojhowe95@gmail.com>
2020-09-28 10:20:53 -0600

    Moved tests

---
+++ src/test/java/be/seeseemelk/mockbukkit/boss/BossBarMockTest.java
@@ -0,0 +1,41 @@
+package be.seeseemelk.mockbukkit.boss;
+
+import org.bukkit.boss.BossBar;
+import org.junit.Before;
+import org.junit.Test;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
+import org.bukkit.boss.BarStyle;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class BossBarMockTest {
+    private ServerMock server;
+
+    @Before
+    public void setUp() {
+        server = MockBukkit.mock();
+    }
+
+    @Test
+    public void testCreateBossBar() {
+        BossBar bar = server.createBossBar("Test bossbar", BarColor.BLUE, BarStyle.SOLID, BarFlag.PLAY_BOSS_MUSIC,
+                BarFlag.CREATE_FOG);
+        assertNotNull(bar);
+
+        assertEquals("Test bossbar", bar.getTitle());
+        assertEquals(BarColor.BLUE, bar.getColor());
+        assertEquals(BarStyle.SOLID, bar.getStyle());
+
+        assertTrue(bar.hasFlag(BarFlag.PLAY_BOSS_MUSIC));
+        assertTrue(bar.hasFlag(BarFlag.CREATE_FOG));
+        assertFalse(bar.hasFlag(BarFlag.DARKEN_SKY));
+
+    }
+}
---
+++ src/test/java/be/seeseemelk/mockbukkit/boss/KeyedBossBarMockTest.java
@@ -0,0 +1,44 @@
+package be.seeseemelk.mockbukkit.boss;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.KeyedBossBar;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.Iterators;
+
+public class KeyedBossBarMockTest {
+    private ServerMock server;
+
+    @Before
+    public void setUp() {
+        server = MockBukkit.mock();
+    }
+
+    @Test
+    @SuppressWarnings("deprecation")
+    public void testKeyedBossBar() {
+        KeyedBossBar bar = server.createBossBar(new NamespacedKey("mockbukkittest", "bossbar1"), "Boss bar 1",
+                BarColor.WHITE, BarStyle.SEGMENTED_10);
+        assertNotNull(bar);
+
+        assertEquals(1, Iterators.size(server.getBossBars()));
+
+        assertEquals(bar, server.getBossBar(new NamespacedKey("mockbukkittest", "bossbar1")));
+
+        assertTrue(server.removeBossBar(new NamespacedKey("mockbukkittest", "bossbar1")));
+        assertEquals(0, Iterators.size(server.getBossBars()));
+
+        assertFalse(server.removeBossBar(new NamespacedKey("mockbukkittest", "nonexistantbossbar")));
+    }
+}
