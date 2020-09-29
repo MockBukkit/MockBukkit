b2e23aa8a002a18b6eb63d1b0e92a15c916d67de
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-09-29 10:56:41 +0200

    Added Shulker Box mocks

--- src/main/java/be/seeseemelk/mockbukkit/block/state/BlockStateMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/BlockStateMock.java
@@ -267,6 +267,24 @@ public class BlockStateMock implements BlockState, Cloneable
 		case WARPED_SIGN:
 		case WARPED_WALL_SIGN:
 			return new SignMock(block);
+		case SHULKER_BOX:
+		case WHITE_SHULKER_BOX:
+		case ORANGE_SHULKER_BOX:
+		case MAGENTA_SHULKER_BOX:
+		case LIGHT_BLUE_SHULKER_BOX:
+		case YELLOW_SHULKER_BOX:
+		case LIME_SHULKER_BOX:
+		case PINK_SHULKER_BOX:
+		case GRAY_SHULKER_BOX:
+		case LIGHT_GRAY_SHULKER_BOX:
+		case CYAN_SHULKER_BOX:
+		case PURPLE_SHULKER_BOX:
+		case BLUE_SHULKER_BOX:
+		case BROWN_SHULKER_BOX:
+		case GREEN_SHULKER_BOX:
+		case RED_SHULKER_BOX:
+		case BLACK_SHULKER_BOX:
+			return new ShulkerBoxMock(block);
 		default:
 			return new BlockStateMock(block);
 		}
---
+++ src/test/java/be/seeseemelk/mockbukkit/block/state/ShulkerBoxMockTest.java
@@ -0,0 +1,116 @@
+package be.seeseemelk.mockbukkit.block.state;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.block.BlockMock;
+
+public class ShulkerBoxMockTest
+{
+
+	private ShulkerBoxMock shulkerBox;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		MockBukkit.mock();
+		shulkerBox = new ShulkerBoxMock(Material.SHULKER_BOX);
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		MockBukkit.unmock();
+	}
+
+	@Test
+	public void testMaterialShulkerBoxBlockState()
+	{
+		Block block = new BlockMock(Material.SHULKER_BOX);
+		assertTrue(block.getState() instanceof ShulkerBox);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testShulkerBoxStateMaterialValidation()
+	{
+		new ShulkerBoxMock(Material.BREAD);
+	}
+
+	@Test
+	public void testHasInventory()
+	{
+		Inventory inventory = shulkerBox.getInventory();
+		assertNotNull(inventory);
+
+		assertEquals(shulkerBox, inventory.getHolder());
+		assertEquals(InventoryType.SHULKER_BOX, inventory.getType());
+	}
+
+	@Test
+	public void testLocking()
+	{
+		String key = "key";
+
+		assertFalse(shulkerBox.isLocked());
+		assertEquals("", shulkerBox.getLock());
+
+		shulkerBox.setLock("key");
+		assertTrue(shulkerBox.isLocked());
+		assertEquals(key, shulkerBox.getLock());
+	}
+
+	@Test
+	public void testNullLocking()
+	{
+		shulkerBox.setLock(null);
+		assertFalse(shulkerBox.isLocked());
+		assertEquals("", shulkerBox.getLock());
+	}
+
+	@Test
+	public void testNaming()
+	{
+		String name = "Cool Shulker";
+
+		assertNull(shulkerBox.getCustomName());
+
+		shulkerBox.setCustomName(name);
+		assertEquals(name, shulkerBox.getCustomName());
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testNullPointerUndyed()
+	{
+		shulkerBox.getColor();
+	}
+
+	@Test
+	public void testShulkerColors()
+	{
+		for (DyeColor color : DyeColor.values())
+		{
+			assertDyed(Material.valueOf(color.name() + "_SHULKER_BOX"), color);
+		}
+	}
+
+	private void assertDyed(Material shulkerBox, DyeColor color)
+	{
+		Block block = new BlockMock(shulkerBox);
+		assertTrue(block.getState() instanceof ShulkerBox);
+		assertEquals(color, ((ShulkerBox) block.getState()).getColor());
+	}
+}
