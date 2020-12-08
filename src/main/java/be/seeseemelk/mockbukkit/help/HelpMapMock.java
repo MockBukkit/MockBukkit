9e91b9bcd01f8bf81ed208a41e15d9bfcb19d91f
NeumimTo <NeumimTo@users.noreply.github.com>
2020-12-08 20:05:04 +0100

    Get rid of * import

--- src/main/java/be/seeseemelk/mockbukkit/help/HelpMapMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/help/HelpMapMock.java
@@ -8,7 +8,11 @@ import org.bukkit.help.HelpTopic;
 import org.bukkit.help.HelpTopicComparator;
 import org.bukkit.help.HelpTopicFactory;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.List;
 
 public class HelpMapMock implements HelpMap
 {
