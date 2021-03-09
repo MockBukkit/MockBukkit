0d3648ad818dc3f376abd753d6409da355b28583
Narimm <benjicharlton@gmail.com>
2021-03-10 07:49:15 +1000

    Update src/main/java/be/seeseemelk/mockbukkit/scheduler/BukkitSchedulerMock.java
    This is why I dont use github codereview....

--- src/main/java/be/seeseemelk/mockbukkit/scheduler/BukkitSchedulerMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/scheduler/BukkitSchedulerMock.java
@@ -1,7 +1,7 @@
 package be.seeseemelk.mockbukkit.scheduler;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
