40b24a4234ba339f845d304facfa5b2a1be4a792
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-03-11 12:07:53 +0100

    Added parts from #187

--- src/main/java/be/seeseemelk/mockbukkit/plugin/PluginManagerMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/plugin/PluginManagerMock.java
@@ -56,25 +56,28 @@ import org.bukkit.plugin.UnknownDependencyException;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 import org.bukkit.plugin.java.JavaPluginUtils;
+import org.jetbrains.annotations.NotNull;
 
 import be.seeseemelk.mockbukkit.ServerMock;
 import be.seeseemelk.mockbukkit.UnimplementedOperationException;
+import be.seeseemelk.mockbukkit.scheduler.BukkitSchedulerMock;
 
 public class PluginManagerMock implements PluginManager
 {
 	private final ServerMock server;
-	private final List<Plugin> plugins = new ArrayList<>();
 	private final JavaPluginLoader loader;
+	private final List<Plugin> plugins = new ArrayList<>();
 	private final List<PluginCommand> commands = new ArrayList<>();
 	private final List<Event> events = new ArrayList<>();
 	private final List<File> temporaryFiles = new LinkedList<>();
-	private final List<Class<?>> pluginConstructorTypes = Arrays.asList(JavaPluginLoader.class,
-	        PluginDescriptionFile.class, File.class, File.class);
 	private final List<Permission> permissions = new ArrayList<>();
 	private final Map<Permissible, Set<String>> permissionSubscriptions = new HashMap<>();
 
+	private final List<Class<?>> pluginConstructorTypes = Arrays.asList(JavaPluginLoader.class,
+	        PluginDescriptionFile.class, File.class, File.class);
+
 	@SuppressWarnings("deprecation")
-	public PluginManagerMock(ServerMock server)
+	public PluginManagerMock(@NotNull ServerMock server)
 	{
 		this.server = server;
 		loader = new JavaPluginLoader(this.server);
@@ -105,7 +108,7 @@ public class PluginManagerMock implements PluginManager
 	 * @param message   The message to display when no event conforms.
 	 * @param predicate The predicate to test against.
 	 */
-	public void assertEventFired(String message, Predicate<Event> predicate)
+	public void assertEventFired(@NotNull String message, @NotNull Predicate<Event> predicate)
 	{
 		for (Event event : events)
 		{
@@ -120,7 +123,7 @@ public class PluginManagerMock implements PluginManager
 	 *
 	 * @param predicate The predicate to test against.
 	 */
-	public void assertEventFired(Predicate<Event> predicate)
+	public void assertEventFired(@NotNull Predicate<Event> predicate)
 	{
 		assertEventFired("Event assert failed", predicate);
 	}
@@ -161,7 +164,7 @@ public class PluginManagerMock implements PluginManager
 	 *
 	 * @param eventClass The class of the event to check for.
 	 */
-	public void assertEventFired(Class<? extends Event> eventClass)
+	public void assertEventFired(@NotNull Class<? extends Event> eventClass)
 	{
 		assertEventFired("No event of that type has been fired", eventClass::isInstance);
 	}
@@ -190,6 +193,7 @@ public class PluginManagerMock implements PluginManager
 	 *
 	 * @return A collection of all available commands.
 	 */
+	@NotNull
 	public Collection<PluginCommand> getCommands()
 	{
 		return Collections.unmodifiableList(commands);
@@ -203,7 +207,7 @@ public class PluginManagerMock implements PluginManager
 	 *                    should be an exact match while the rest don't have to be.
 	 * @return {@code true} if the constructor is compatible, {@code false} if it isn't.
 	 */
-	private boolean isConstructorCompatible(Constructor<?> constructor, Class<?>[] types)
+	private boolean isConstructorCompatible(@NotNull Constructor<?> constructor, @NotNull Class<?>[] types)
 	{
 		Class<?>[] parameters = constructor.getParameterTypes();
 		for (int i = 0; i < types.length; i++)
@@ -262,7 +266,8 @@ public class PluginManagerMock implements PluginManager
 	 * @return The created temporary directory.
 	 * @throws IOException when the directory could not be created.
 	 */
-	private File createTemporaryDirectory(String name) throws IOException
+	@NotNull
+	private File createTemporaryDirectory(@NotNull String name) throws IOException
 	{
 		Random random = ThreadLocalRandom.current();
 		File directory = Files.createTempDirectory(name + "-" + random.nextInt()).toFile();
@@ -275,7 +280,7 @@ public class PluginManagerMock implements PluginManager
 	 *
 	 * @param plugin The plugin that has been loaded.
 	 */
-	public void registerLoadedPlugin(Plugin plugin)
+	public void registerLoadedPlugin(@NotNull Plugin plugin)
 	{
 		addCommandsFrom(plugin);
 		plugins.add(plugin);
@@ -381,8 +386,13 @@ public class PluginManagerMock implements PluginManager
 	}
 
 	@Override
-	public void callEvent(Event event) throws IllegalStateException
+	public void callEvent(@NotNull Event event)
 	{
+		if (event.isAsynchronous() && server.isOnMainThread())
+		{
+			throw new IllegalStateException("Asynchronous Events cannot be called on the main Thread.");
+		}
+		
 		events.add(event);
 		HandlerList handlers = event.getHandlers();
 		RegisteredListener[] listeners = handlers.getRegisteredListeners();
@@ -392,7 +402,24 @@ public class PluginManagerMock implements PluginManager
 		}
 	}
 
-	private void callRegisteredListener(RegisteredListener registration, Event event)
+	/**
+	 * This method invokes {@link #callEvent(Event)} from a different {@link Thread}
+	 * using the {@link BukkitSchedulerMock}.
+	 * 
+	 * @param event The asynchronous {@link Event} to call.
+	 */
+	public void callEventAsynchronously(@NotNull Event event)
+	{
+		if (!event.isAsynchronous())
+		{
+			throw new IllegalStateException("Synchronous Events cannot be called asynchronously.");
+		}
+
+		// Our Scheduler will call the Event on a dedicated Event Thread Executor
+		server.getScheduler().scheduleEventCall(() -> callEvent(event));
+	}
+
+	private void callRegisteredListener(@NotNull RegisteredListener registration, @NotNull Event event)
 	{
 		if (!registration.getPlugin().isEnabled())
 		{
@@ -684,8 +711,7 @@ public class PluginManagerMock implements PluginManager
 	@Override
 	public void removePermission(Permission perm)
 	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
+		permissions.remove(perm);
 	}
 
 	@Override
