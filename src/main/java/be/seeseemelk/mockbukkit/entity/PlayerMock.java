31020ed0eda572bb8ab60a5c8d8e2be141e0171b
AyBiCi <rymcymcym100@gmail.com>
2021-04-20 02:47:57 +0200

    Included hashmap

--- src/main/java/be/seeseemelk/mockbukkit/entity/PlayerMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/entity/PlayerMock.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.HashMap;
 import java.util.function.Predicate;
 
 import org.bukkit.BanList;
