35b5fee47d10d46f10ff2f8c223701e84e607e7a
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-05-04 10:49:07 +0200

    Removed deprecated method

--- src/test/java/be/seeseemelk/mockbukkit/CoordinateTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/CoordinateTest.java
@@ -3,7 +3,6 @@ package be.seeseemelk.mockbukkit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
