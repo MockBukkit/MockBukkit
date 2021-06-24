d2d3de6e410ff3c9d102b6f083a90c0b564c7ac1
okx-code <okx@okx.sh>
2021-06-24 04:25:26 +0100

    use tabs

--- src/main/java/be/seeseemelk/mockbukkit/statistic/StatisticMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/statistic/StatisticMock.java
@@ -10,47 +10,46 @@ import org.bukkit.entity.EntityType;
 
 public class StatisticMock
 {
-  private final Map<Statistic, Integer> untypedStatistics = new Hashtable<>();
-  private final Map<Statistic, Map<Material, Integer>> materialStatistics = new HashMap<>();
-  private final Map<Statistic, Map<EntityType, Integer>> entityStatistics = new HashMap<>();
 
-  public void setStatistic(Statistic statistic, int amount)
-  {
-    untypedStatistics.put(statistic, amount);
-  }
+	private final Map<Statistic, Integer> untypedStatistics = new Hashtable<>();
+	private final Map<Statistic, Map<Material, Integer>> materialStatistics = new HashMap<>();
+	private final Map<Statistic, Map<EntityType, Integer>> entityStatistics = new HashMap<>();
 
-  public void setStatistic(Statistic statistic, Material material, int amount)
-  {
-    materialStatistics.computeIfAbsent(statistic, k -> new HashMap<>()).put(material, amount);
-  }
-
-  public void setStatistic(Statistic statistic, EntityType entity, int amount)
-  {
-    entityStatistics.computeIfAbsent(statistic, k -> new HashMap<>()).put(entity, amount);
-  }
-
-  public int getStatistic(Statistic statistic)
-  {
-    return untypedStatistics.getOrDefault(statistic, 0);
-  }
-
-  public int getStatistic(Statistic statistic, Material material)
-  {
-	Map<Material, Integer> map = materialStatistics.get(statistic);
-	if (map == null)
+	public void setStatistic(Statistic statistic, int amount)
 	{
-	  return 0;
+		untypedStatistics.put(statistic, amount);
 	}
-	return map.getOrDefault(material, 0);
-  }
 
-  public int getStatistic(Statistic statistic, EntityType entity)
-  {
-	Map<EntityType, Integer> map = entityStatistics.get(statistic);
-	if (map == null)
+	public void setStatistic(Statistic statistic, Material material, int amount)
 	{
-	  return 0;
+		materialStatistics.computeIfAbsent(statistic, k -> new HashMap<>()).put(material, amount);
 	}
-	return map.getOrDefault(entity, 0);
-  }
+
+	public void setStatistic(Statistic statistic, EntityType entity, int amount)
+	{
+		entityStatistics.computeIfAbsent(statistic, k -> new HashMap<>()).put(entity, amount);
+	}
+
+	public int getStatistic(Statistic statistic)
+	{
+		return untypedStatistics.getOrDefault(statistic, 0);
+	}
+
+	public int getStatistic(Statistic statistic, Material material)
+	{
+		Map<Material, Integer> map = materialStatistics.get(statistic);
+		if (map == null) {
+			return 0;
+		}
+		return map.getOrDefault(material, 0);
+	}
+
+	public int getStatistic(Statistic statistic, EntityType entity)
+	{
+		Map<EntityType, Integer> map = entityStatistics.get(statistic);
+		if (map == null) {
+			return 0;
+		}
+		return map.getOrDefault(entity, 0);
+	}
 }
