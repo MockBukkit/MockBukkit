568692e44d51e02d053f725d5f57de77fd5397e7
Seggan <segganew@gmail.com>
2021-07-26 15:50:06 -0400

    Tests

--- src/test/java/be/seeseemelk/mockbukkit/MockChunkDataTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/MockChunkDataTest.java
@@ -50,4 +50,49 @@ public class MockChunkDataTest
 
 		Assertions.assertEquals(Material.AIR, data.getType(33, 1000, 33));
 	}
+
+	@Test
+	void test_neg_min_height()
+	{
+		MinHeightMock dummy = new MinHeightMock(-60, 256);
+		ChunkGenerator.ChunkData data = server.createChunkData(dummy);
+
+		data.setBlock(0, -40, 0, Material.OBSIDIAN);
+		Assertions.assertEquals(Material.OBSIDIAN, data.getType(0, -40, 0));
+	}
+
+	@Test
+	void test_pos_min_height()
+	{
+		MinHeightMock dummy = new MinHeightMock(60, 256);
+		ChunkGenerator.ChunkData data = server.createChunkData(dummy);
+
+		data.setBlock(0, 80, 0, Material.OBSIDIAN);
+		Assertions.assertEquals(Material.OBSIDIAN, data.getType(0, 80, 0));
+	}
+
+	private static final class MinHeightMock extends WorldMock
+	{
+		private final int minHeight;
+		private final int maxHeight;
+
+		private MinHeightMock(int minHeight, int maxHeight)
+		{
+			super(Material.GRASS, 2);
+			this.minHeight = minHeight;
+			this.maxHeight = maxHeight;
+		}
+
+		@Override
+		public int getMinHeight()
+		{
+			return minHeight;
+		}
+
+		@Override
+		public int getMaxHeight()
+		{
+			return maxHeight;
+		}
+	}
 }
