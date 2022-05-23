b010812380f8913dba3d69f0bc73c00ba51af324
Pierce Thompson <insprill@gmail.com>
2022-05-22 22:11:55 -0400

    Fix scoreboard tests failing

--- src/main/java/be/seeseemelk/mockbukkit/scoreboard/ScoreboardManagerMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/scoreboard/ScoreboardManagerMock.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class ScoreboardManagerMock implements ScoreboardManager
 {
+
 	private final ScoreboardMock mainScoreboard = new ScoreboardMock();
 
 	@Override
