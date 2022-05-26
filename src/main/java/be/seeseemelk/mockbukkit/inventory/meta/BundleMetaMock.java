0e1f5f6ab7e04df6a76d2856c1e8f55e2b06c5d9
Pierce Thompson <insprill@gmail.com>
2022-05-25 22:39:50 -0400

    Create initial BundleMeta mock

---
+++ src/main/java/be/seeseemelk/mockbukkit/inventory/meta/BundleMetaMock.java
@@ -0,0 +1,41 @@
+package be.seeseemelk.mockbukkit.inventory.meta;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BundleMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class BundleMetaMock extends ItemMetaMock implements BundleMeta
+{
+
+	@Override
+	public boolean hasItems()
+	{
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public @NotNull List<ItemStack> getItems()
+	{
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setItems(@Nullable List<ItemStack> items)
+	{
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void addItem(@NotNull ItemStack item)
+	{
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+}
