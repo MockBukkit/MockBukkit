e643a249a7125c0c320936627584b740f0393980
Pierce Thompson <insprill@gmail.com>
2022-05-29 20:46:41 -0400

    Fix missing import

--- src/test/java/be/seeseemelk/mockbukkit/WorldMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/WorldMockTest.java
@@ -32,6 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
