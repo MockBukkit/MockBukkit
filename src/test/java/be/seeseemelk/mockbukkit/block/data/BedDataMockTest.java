74aca573ced97c9a6a4405b5e2a4b69876899dfb
Pierce Thompson <insprill@gmail.com>
2022-06-01 22:38:25 -0400

    Remove BedDataMockTest public modifier

--- src/test/java/be/seeseemelk/mockbukkit/block/data/BedDataMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/block/data/BedDataMockTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class BedDataMockTest
+class BedDataMockTest
 {
 
 	@Test
