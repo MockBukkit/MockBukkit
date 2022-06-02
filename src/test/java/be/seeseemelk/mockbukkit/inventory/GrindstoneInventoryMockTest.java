721f2d2a5bb0f878eef38de11bb5f6818e234e0d
Pierce Thompson <insprill@gmail.com>
2022-06-01 21:50:47 -0400

    Use junit assertions

--- src/test/java/be/seeseemelk/mockbukkit/inventory/GrindstoneInventoryMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/inventory/GrindstoneInventoryMockTest.java
@@ -5,10 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalToObject;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class GrindstoneInventoryMockTest
 {
@@ -33,8 +31,8 @@ class GrindstoneInventoryMockTest
 	{
 		InventoryMock snapshot = inventory.getSnapshot();
 
-		assertThat(snapshot, instanceOf(GrindstoneInventoryMock.class));
-		assertThat(snapshot, not(equalToObject(inventory)));
+		assertInstanceOf(GrindstoneInventoryMock.class, snapshot);
+		assertNotEquals(inventory, snapshot);
 	}
 
 }
