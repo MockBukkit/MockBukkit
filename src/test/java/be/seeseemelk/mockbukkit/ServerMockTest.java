4f15ae36b2b810643c564de1d82863e8abfc2ca3
Peaches_MLG <PeachesMLG100@gmail.com>
2022-06-02 23:15:23 +0100

    Update src/test/java/be/seeseemelk/mockbukkit/ServerMockTest.java
    Co-authored-by: Pierce Thompson <52843537+Insprill@users.noreply.github.com>

--- src/test/java/be/seeseemelk/mockbukkit/ServerMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/ServerMockTest.java
@@ -649,8 +649,8 @@ class ServerMockTest
 	{
 		MockBukkit.load(TestPlugin.class);
 		Player player = server.addPlayer();
-		assertEquals(Arrays.asList("Tab", "Complete", "Results"), server.getCommandTabComplete(player, "mockcommand", new String[]{ "argA" }));
-		assertEquals(Arrays.asList("Other", "Results"), server.getCommandTabComplete(player, "mockcommand", new String[]{ "argA", "argB" }));
+		assertEquals(Arrays.asList("Other", "Results"), server.getCommandTabComplete(player, "mockcommand argA"));
+		assertEquals(Arrays.asList("Tab", "Complete", "Results"), server.getCommandTabComplete(player, "mockcommand argA argB"));
 	}
 
 }
