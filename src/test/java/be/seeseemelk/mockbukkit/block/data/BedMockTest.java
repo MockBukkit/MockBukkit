df02b3539438f9d5c0702b04425d1b7a9bacd35e
Pierce Thompson <insprill@gmail.com>
2022-06-02 20:48:46 -0400

    Update tests

--- src/test/java/be/seeseemelk/mockbukkit/block/data/BedMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/block/data/BedMockTest.java
@@ -3,30 +3,68 @@ package be.seeseemelk.mockbukkit.block.data;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.type.Bed;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class BedMockTest
 {
 
+	private BedMock bed;
+
+	@BeforeEach
+	public void setUp()
+	{
+		this.bed = new BedMock(Material.RED_BED);
+	}
+
 	@Test
 	void constructor_DefaultValues()
 	{
-		BedMock bed = new BedMock(Material.RED_BED);
-
 		assertEquals(Bed.Part.FOOT, bed.getPart());
 		assertFalse(bed.isOccupied());
 		assertEquals(BlockFace.NORTH, bed.getFacing());
 	}
 
 	@Test
-	void getAsString_NoData()
+	void setFacing_Valid()
 	{
-		BedMock bed = new BedMock(Material.RED_BED);
+		for (BlockFace face : BlockFace.values())
+		{
+			if (!bed.getFaces().contains(face))
+				continue;
+			assertDoesNotThrow(() -> bed.setFacing(face));
+		}
+	}
 
+	@Test
+	void setFacing_Invalid()
+	{
+		for (BlockFace face : BlockFace.values())
+		{
+			if (bed.getFaces().contains(face))
+				continue;
+			assertThrowsExactly(IllegalArgumentException.class, () -> bed.setFacing(face));
+		}
+	}
+
+	@Test
+	void getFaces()
+	{
+		Set<BlockFace> validFaces = Set.of(BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST);
+		assertEquals(validFaces, bed.getFaces());
+	}
+
+	@Test
+	void getAsString()
+	{
 		assertEquals("minecraft:red_bed[facing=north,occupied=false,part=foot]", bed.getAsString());
 	}
 
