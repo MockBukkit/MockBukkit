6a94511eadb4671f06b1331eba6f943f20fe42f1
Pierce Thompson <insprill@gmail.com>
2022-06-03 23:22:20 -0400

    Implement Brewing Stand BlockState

---
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/BrewingStandMock.java
@@ -0,0 +1,88 @@
+package be.seeseemelk.mockbukkit.block.state;
+
+import be.seeseemelk.mockbukkit.inventory.InventoryMock;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.BrewingStand;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.BrewerInventory;
+import org.jetbrains.annotations.NotNull;
+
+public class BrewingStandMock extends ContainerMock implements BrewingStand
+{
+
+	private Component customName;
+	private int brewingTime;
+	private int fuelLevel;
+
+	protected BrewingStandMock(@NotNull Material material)
+	{
+		super(material);
+		if (material != Material.BREWING_STAND)
+			throw new IllegalArgumentException("Cannot create a Brewing Stand state from " + material);
+	}
+
+	protected BrewingStandMock(@NotNull Block block)
+	{
+		super(block);
+		if (block.getType() != Material.BREWING_STAND)
+			throw new IllegalArgumentException("Cannot create a Brewing Stand state from " + block.getType());
+	}
+
+	protected BrewingStandMock(@NotNull ContainerMock state)
+	{
+		super(state);
+	}
+
+	@Override
+	protected InventoryMock createInventory()
+	{
+//		return new BrewingStandInventory(this); TODO: Not implemented.
+		return new InventoryMock(this, InventoryType.BREWING);
+	}
+
+	@Override
+	public @NotNull BlockState getSnapshot()
+	{
+		return new BrewingStandMock(this);
+	}
+
+	@Override
+	public int getBrewingTime()
+	{
+		return this.brewingTime;
+	}
+
+	@Override
+	public void setBrewingTime(int brewTime)
+	{
+		this.brewingTime = brewTime;
+	}
+
+	@Override
+	public int getFuelLevel()
+	{
+		return this.fuelLevel;
+	}
+
+	@Override
+	public void setFuelLevel(int level)
+	{
+		this.fuelLevel = level;
+	}
+
+	@Override
+	public @NotNull BrewerInventory getInventory()
+	{
+		return (BrewerInventory) super.getInventory();
+	}
+
+	@Override
+	public @NotNull BrewerInventory getSnapshotInventory()
+	{
+		return (BrewerInventory) super.getSnapshotInventory();
+	}
+
+}
