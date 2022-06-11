2ff2ea0191cde020f09af05fa420328f3429da21
Pierce Thompson <insprill@gmail.com>
2022-06-11 12:28:39 -0400

    Add ComparatorMock

---
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/ComparatorMock.java
@@ -0,0 +1,38 @@
+package be.seeseemelk.mockbukkit.block.state;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Comparator;
+import org.jetbrains.annotations.NotNull;
+
+public class ComparatorMock extends TileStateMock implements Comparator
+{
+
+
+	protected ComparatorMock(@NotNull Material material)
+	{
+		super(material);
+		if (material != Material.COMPARATOR)
+			throw new IllegalArgumentException("Cannot create a Comparator state from " + material);
+	}
+
+	protected ComparatorMock(@NotNull Block block)
+	{
+		super(block);
+		if (block.getType() != Material.COMPARATOR)
+			throw new IllegalArgumentException("Cannot create a Comparator state from " + block.getType());
+	}
+
+	protected ComparatorMock(@NotNull ComparatorMock state)
+	{
+		super(state);
+	}
+
+	@Override
+	public @NotNull BlockState getSnapshot()
+	{
+		return new ComparatorMock(this);
+	}
+
+}
