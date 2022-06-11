dfdbe1a8ed9a3555ffd7fe7ecc246c6c2a0e57bf
Pierce Thompson <insprill@gmail.com>
2022-06-11 12:30:18 -0400

    Add ComparatorMock to BlockState#mockState

--- src/main/java/be/seeseemelk/mockbukkit/block/state/BlockStateMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/BlockStateMock.java
@@ -250,6 +250,8 @@ public class BlockStateMock implements BlockState
 	{
 		switch (block.getType())
 		{
+		case COMPARATOR:
+			return new ComparatorMock(block);
 		case CAMPFIRE:
 		case SOUL_CAMPFIRE:
 			return new CampfireMock(block);
