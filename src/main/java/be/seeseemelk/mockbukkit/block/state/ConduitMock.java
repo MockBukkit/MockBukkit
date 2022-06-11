4955757ff9090b71a00397a8eb8be4dfe0c69fb1
Pierce Thompson <insprill@gmail.com>
2022-06-11 12:41:04 -0400

    Add ConduitMock

---
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/ConduitMock.java
@@ -0,0 +1,37 @@
+package be.seeseemelk.mockbukkit.block.state;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Comparator;
+import org.jetbrains.annotations.NotNull;
+
+public class ConduitMock extends TileStateMock implements Comparator
+{
+
+	protected ConduitMock(@NotNull Material material)
+	{
+		super(material);
+		if (material != Material.CONDUIT)
+			throw new IllegalArgumentException("Cannot create a Comparator state from " + material);
+	}
+
+	protected ConduitMock(@NotNull Block block)
+	{
+		super(block);
+		if (block.getType() != Material.CONDUIT)
+			throw new IllegalArgumentException("Cannot create a Comparator state from " + block.getType());
+	}
+
+	protected ConduitMock(@NotNull ConduitMock state)
+	{
+		super(state);
+	}
+
+	@Override
+	public @NotNull BlockState getSnapshot()
+	{
+		return new ConduitMock(this);
+	}
+
+}
