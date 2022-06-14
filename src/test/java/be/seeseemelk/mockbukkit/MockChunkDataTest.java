fa963a57d73db1b25220c120b815d0cd3a85a531
Pierce Thompson <insprill@gmail.com>
2022-06-14 15:38:27 -0400

    Fix merge error

--- src/test/java/be/seeseemelk/mockbukkit/MockChunkDataTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/MockChunkDataTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class MockChunkDataTest
 class MockChunkDataTest
 {
 
