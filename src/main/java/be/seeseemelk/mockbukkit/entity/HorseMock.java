d4ed7ffe21cca5c2d00d3392bf078ae3661d46ad
Justin Frahm <justinmfrahm@gmail.com>
2022-06-25 15:32:25 -0600

    Add HorseMock

---
+++ src/main/java/be/seeseemelk/mockbukkit/entity/HorseMock.java
@@ -0,0 +1,64 @@
+package be.seeseemelk.mockbukkit.entity;
+
+import java.util.UUID;
+
+import org.bukkit.entity.Horse;
+import org.bukkit.inventory.HorseInventory;
+import org.jetbrains.annotations.NotNull;
+
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.UnimplementedOperationException;
+import be.seeseemelk.mockbukkit.inventory.HorseInventoryMock;
+
+public class HorseMock extends AbstractHorseMock implements Horse {
+    private Color color;
+    private Style style;
+    private HorseInventory inventory;
+
+    public HorseMock(ServerMock server, UUID uuid) {
+        super(server, uuid);
+        inventory = new HorseInventoryMock(this);
+    }
+
+    @Override
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public Style getStyle() {
+        return style;
+    }
+
+    @Override
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+
+    @Override
+    public boolean isCarryingChest() {
+        throw new UnimplementedOperationException();
+    }
+
+    @Override
+    public void setCarryingChest(boolean chest) {
+        throw new UnimplementedOperationException();
+    }
+
+    @Override
+    public HorseInventory getInventory() {
+        return inventory;
+    }
+
+    @Deprecated
+    @Override
+    public @NotNull Variant getVariant() {
+        // TODO Auto-generated method stub
+        throw new UnimplementedOperationException();
+    }
+}
