cfaeef18b5f9ee8ffbc96c7fc2cd62f4296589e0
Justin Frahm <justinmfrahm@gmail.com>
2022-06-25 17:57:23 -0600

    Fix space/tab, add stateChecks

--- src/main/java/be/seeseemelk/mockbukkit/entity/HorseMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/entity/HorseMock.java
@@ -13,59 +13,62 @@ import be.seeseemelk.mockbukkit.UnimplementedOperationException;
 import be.seeseemelk.mockbukkit.inventory.HorseInventoryMock;
 
 public class HorseMock extends AbstractHorseMock implements Horse {
-    private Color color;
-    private Style style;
-    private HorseInventory inventory;
+	private Color color;
+	private Style style;
+	private HorseInventory inventory;
 
-    public HorseMock(ServerMock server, UUID uuid) {
-        super(server, uuid);
-        inventory = new HorseInventoryMock(this);
-        color = Color.WHITE;
-        style = Style.BLACK_DOTS;
-        inventory = new HorseInventoryMock(this);
-    }
+	public HorseMock(ServerMock server, UUID uuid) {
+		super(server, uuid);
+		inventory = new HorseInventoryMock(this);
+		color = Color.WHITE;
+		style = Style.BLACK_DOTS;
+		inventory = new HorseInventoryMock(this);
+	}
 
-    @Override
-    public Color getColor() {
-        return this.color;
-    }
+	@Override
+	public Color getColor() {
+		Preconditions.checkState(this.color != null, "No color has been set");
+		return this.color;
+	}
 
-    @Override
-    public void setColor(@NotNull Color color) {
-        Preconditions.checkNotNull(color,"Color cannot be null");
-        this.color = color;
-    }
+	@Override
+	public void setColor(@NotNull Color color) {
+		Preconditions.checkNotNull(color, "Color cannot be null");
+		this.color = color;
+	}
 
-    @Override
-    public Style getStyle() {
-        return this.style;
-    }
+	@Override
+	public Style getStyle() {
+		Preconditions.checkState(this.style != null, "No style has been set");
+		return this.style;
+	}
 
-    @Override
-    public void setStyle(@NotNull Style style) {
-        Preconditions.checkNotNull(style,"Style cannot be null");
-        this.style = style;
-    }
+	@Override
+	public void setStyle(@NotNull Style style) {
+		Preconditions.checkNotNull(style,"Style cannot be null");
+		this.style = style;
+	}
 
-    @Override
-    public boolean isCarryingChest() {
-        throw new UnimplementedOperationException();
-    }
+	@Override
+	public boolean isCarryingChest() {
+		throw new UnimplementedOperationException();
+	}
 
-    @Override
-    public void setCarryingChest(boolean chest) {
-        throw new UnimplementedOperationException();
-    }
+	@Override
+	public void setCarryingChest(boolean chest) {
+		throw new UnimplementedOperationException();
+	}
 
-    @Override
-    public HorseInventory getInventory() {
-        return this.inventory;
-    }
+	@Override
+	public HorseInventory getInventory() {
+		Preconditions.checkState(this.inventory != null, "No inventory has been set");
+		return this.inventory;
+	}
 
-    @Deprecated
-    @Override
-    public @NotNull Variant getVariant() {
-        // TODO Auto-generated method stub
-        throw new UnimplementedOperationException();
-    }
+	@Deprecated
+	@Override
+	public @NotNull Variant getVariant() {
+		// TODO Auto-generated method stub
+		throw new UnimplementedOperationException();
+	}
 }
--- src/main/java/be/seeseemelk/mockbukkit/inventory/HorseInventoryMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/inventory/HorseInventoryMock.java
@@ -7,27 +7,27 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 public class HorseInventoryMock extends InventoryMock implements HorseInventory {
-    public HorseInventoryMock(InventoryHolder holder) {
-        super(holder, 2, InventoryType.CHEST);
-    }
+	public HorseInventoryMock(InventoryHolder holder) {
+		super(holder, 2, InventoryType.CHEST);
+	}
 
-    @Override
-    public ItemStack getSaddle() {
-        return this.getItem(0);
-    }
+	@Override
+	public ItemStack getSaddle() {
+		return this.getItem(0);
+	}
 
-    @Override
-    public void setSaddle(@Nullable ItemStack stack) {
-        this.setItem(0, stack);
-    }
+	@Override
+	public void setSaddle(@Nullable ItemStack stack) {
+		this.setItem(0, stack);
+	}
 
-    @Override
-    public ItemStack getArmor() {
-        return this.getItem(1);
-    }
+	@Override
+	public ItemStack getArmor() {
+		return this.getItem(1);
+	}
 
-    @Override
-    public void setArmor(@Nullable ItemStack stack) {
-        this.setItem(1, stack);
-    }
+	@Override
+	public void setArmor(@Nullable ItemStack stack) {
+		this.setItem(1, stack);
+	}
 }
