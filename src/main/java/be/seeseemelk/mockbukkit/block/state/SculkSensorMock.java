1e66f357ec8142981bad77471d653141ad43f372
Pierce Thompson <insprill@gmail.com>
2022-06-28 16:03:06 -0400

    Implement the SculkSensorMock BlockState

---
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/SculkSensorMock.java
@@ -0,0 +1,68 @@
+package be.seeseemelk.mockbukkit.block.state;
+
+import com.google.common.base.Preconditions;
+import org.bukkit.Material;
+import org.bukkit.block.Bell;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.SculkSensor;
+import org.jetbrains.annotations.NotNull;
+
+public class SculkSensorMock extends TileStateMock implements SculkSensor
+{
+
+	private int lastVibrationFrequency;
+	private int listenerRange;
+
+	public SculkSensorMock(@NotNull Material material)
+	{
+		super(material);
+		checkType(material, Material.SCULK_SENSOR);
+	}
+
+	protected SculkSensorMock(@NotNull Block block)
+	{
+		super(block);
+		checkType(block, Material.SCULK_SENSOR);
+	}
+
+	protected SculkSensorMock(@NotNull SculkSensorMock state)
+	{
+		super(state);
+		this.lastVibrationFrequency = state.lastVibrationFrequency;
+		this.listenerRange = state.listenerRange;
+	}
+
+	@Override
+	public @NotNull BlockState getSnapshot()
+	{
+		return new SculkSensorMock(this);
+	}
+
+	@Override
+	public int getLastVibrationFrequency()
+	{
+		return this.lastVibrationFrequency;
+	}
+
+	@Override
+	public void setLastVibrationFrequency(int lastVibrationFrequency)
+	{
+		Preconditions.checkArgument(0 <= lastVibrationFrequency && lastVibrationFrequency <= 15, "Vibration frequency must be between 0-15");
+		this.lastVibrationFrequency = lastVibrationFrequency;
+	}
+
+	@Override
+	public int getListenerRange()
+	{
+		return this.listenerRange;
+	}
+
+	@Override
+	public void setListenerRange(int range)
+	{
+		Preconditions.checkArgument(range > 0, "Vibration listener range must be greater than 0");
+		this.listenerRange = range;
+	}
+
+}
