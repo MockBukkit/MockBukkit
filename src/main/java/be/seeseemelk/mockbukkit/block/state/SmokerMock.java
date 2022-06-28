e2be51bd54e9d09454415de59e670c563e6ec8bb
Pierce Thompson <insprill@gmail.com>
2022-06-28 17:26:40 -0400

    Leave inventory handling to the AbstractFurnaceMock

--- src/main/java/be/seeseemelk/mockbukkit/block/state/SmokerMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/SmokerMock.java
@@ -1,7 +1,5 @@
 package be.seeseemelk.mockbukkit.block.state;
 
-import be.seeseemelk.mockbukkit.inventory.InventoryMock;
-import be.seeseemelk.mockbukkit.inventory.FurnaceInventoryMock;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
@@ -21,12 +19,6 @@ public class SmokerMock extends AbstractFurnaceMock implements Smoker
 	{
 		super(block);
 		checkType(block, Material.SMOKER);
-	}
-
-	@Override
-	protected @NotNull InventoryMock createInventory()
-	{
-		return new FurnaceInventoryMock(this);
 	}
 
 	protected SmokerMock(@NotNull SmokerMock state)
