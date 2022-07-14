2bb9b54deace9db9f61be7610892ef11051de48b
Lena Kolb <lenakolb2204@gmail.com>
2022-07-14 18:45:09 +0200

    Add Tests for BeaconInventoryMock

---
+++ src/test/java/be/seeseemelk/mockbukkit/inventory/BeaconInventoryMockTest.java
@@ -0,0 +1,12 @@
+package be.seeseemelk.mockbukkit.inventory;
+
+import be.seeseemelk.mockbukkit.ServerMock;
+
+public class BeaconInventoryMockTest
+{
+
+	ServerMock server;
+	BeaconInventoryMockTest inventory;
+
+
+}
