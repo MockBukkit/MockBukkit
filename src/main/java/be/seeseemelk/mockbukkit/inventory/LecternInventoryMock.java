1ca9ca74477e3d7ee19cec0c5a4f203476af71d3
Lena Kolb <lenakolb2204@gmail.com>
2022-07-18 01:09:42 +0200

    Bring Old InventoryMocks in Line with new ones

--- src/main/java/be/seeseemelk/mockbukkit/inventory/LecternInventoryMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/inventory/LecternInventoryMock.java
@@ -1,15 +1,20 @@
 package be.seeseemelk.mockbukkit.inventory;
 
+import org.bukkit.block.Lectern;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.LecternInventory;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * This {@link InventoryMock} mocks an {@link org.bukkit.block.Lectern} but pretty much behaves like any small chest.
  */
-public class LecternInventoryMock extends InventoryMock
+public class LecternInventoryMock extends InventoryMock implements LecternInventory
 {
+
+	private @Nullable Lectern holder;
 
 	public LecternInventoryMock(InventoryHolder holder)
 	{
@@ -23,6 +28,12 @@ public class LecternInventoryMock extends InventoryMock
 		Inventory inventory = new LecternInventoryMock(getHolder());
 		inventory.setContents(getContents());
 		return inventory;
+	}
+
+	@Override
+	public @Nullable Lectern getHolder()
+	{
+		return (Lectern) super.getHolder();
 	}
 
 }
