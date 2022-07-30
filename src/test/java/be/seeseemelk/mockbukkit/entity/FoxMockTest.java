1e5a55de3bf921d8e5f37f00960e71c5a84eb47a
Lena Kolb <lenakolb2204@gmail.com>
2022-07-30 21:53:42 +0200

    Remove Public Modifier

--- src/test/java/be/seeseemelk/mockbukkit/entity/FoxMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/entity/FoxMockTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FoxMockTest
+class FoxMockTest
 {
 
 	private ServerMock server;
