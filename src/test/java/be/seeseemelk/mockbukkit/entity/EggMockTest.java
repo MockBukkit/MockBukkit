9411f30e544806b7f568a15ad2692d634e55433f
Lena Kolb <lenakolb2204@gmail.com>
2022-08-11 21:32:22 +0200

    Fix Code Smell

--- src/test/java/be/seeseemelk/mockbukkit/entity/EggMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/entity/EggMockTest.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class EggMockTest
+class EggMockTest
 {
 
 	private EggMock egg;
