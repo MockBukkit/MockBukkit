02c959c599ad49587665c01490496e8ba2967bc5
thelooter <lenakolb2204@gmail.com>
2022-08-13 00:34:07 +0200

    Update src/main/java/be/seeseemelk/mockbukkit/entity/PigMock.java
    Co-authored-by: Pierce Thompson <52843537+Insprill@users.noreply.github.com>

--- src/main/java/be/seeseemelk/mockbukkit/entity/PigMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/entity/PigMock.java
@@ -60,7 +60,7 @@ public class PigMock extends AnimalsMock implements Pig
 		{
 			return;
 		}
-		Preconditions.checkArgument(ticks >= 0 && ticks <= this.boostTicks, "boost ticks must not exceed 0 or %d (inclusive)", this.boostTicks);
+		Preconditions.checkArgument(ticks >= 0 && ticks <= this.boostTicks, "Current Boost Ticks must be less than Boost Ticks (#getBoostTicks)");
 		this.currentBoostTicks = ticks;
 	}
 
