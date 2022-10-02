94c962acdd4b365443a0c875dd008587a17573ed
Pierce Thompson <52843537+Insprill@users.noreply.github.com>
2022-10-02 07:45:41 -0400

    Code style

--- src/test/java/be/seeseemelk/mockbukkit/block/data/StairsMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/block/data/StairsMockTest.java
@@ -22,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StairsMockTest
 {
+
 	private static final Set<BlockFace> VALID_FACES = Set.of(
 			BlockFace.NORTH,
 			BlockFace.SOUTH,
