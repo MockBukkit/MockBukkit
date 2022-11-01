55a0bff3a084672702f67b3bb8ec9a392f7dae7f
spscally <scallys96@gmail.com>
2022-11-01 19:58:02 -0400

    Implement resolution of MockBukkitServer-annotated ServerMock parameters

--- src/main/java/be/seeseemelk/mockbukkit/MockBukkitExtension.java
+++ src/main/java/be/seeseemelk/mockbukkit/MockBukkitExtension.java
@@ -1,44 +1,116 @@
 package be.seeseemelk.mockbukkit;
 
-import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.ApiStatus.Experimental;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+import java.lang.reflect.Parameter;
 
 /**
- * Extension that mocks the Bukkit singleton before each test and subsequently unmocks it after each test.
+ * Extension that mocks the Bukkit singleton before each test and subsequently unmocks it after each test. It will also
+ * inject an instance of {@link ServerMock} to any field or parameter annotated with {@link MockBukkitServer}.
  *
- * Example usage:
+ * <p>Example field usage:</p>
  *
  * <pre class="code"><code class="java">
  * <b>&#064;ExtendWith(MockBukkitExtension.class)</b>
- * class ExampleTest
+ * class FieldExampleTest
  * {
+ *
+ * 	&#064;MockBukkitServer
+ * 	private ServerMock serverMock;
  *
  * 	&#064;Test
  * 	void aUnitTest()
- *	{
- *	    ServerMock serverMock = MockBukkit.getMock();
- *	    // ...
- *	}
+ *    {
+ * 	    assert serverMock != null;
+ * 	    // ...
+ *    }
+ *
+ * }
+ * </code></pre>
+ * <p>
+ * Example constructor parameter usage:
+ *
+ * <pre class="code"><code class="java">
+ * <b>&#064;ExtendWith(MockBukkitExtension.class)</b>
+ * class ConstructorExampleTest
+ * {
+ *
+ * 	private ServerMock serverMock;
+ *
+ * 	public ConstructorExampleTest(&#064;MockBukkitServer ServerMock serverMock)
+ *    {
+ * 		this.serverMock = serverMock;
+ *    }
+ *
+ * 	&#064;Test
+ * 	void aUnitTest()
+ *    {
+ * 		assert serverMock != null;
+ * 		// ...
+ *    }
+ *
+ * }
+ * </code></pre>
+ * <p>
+ * Example method parameter usage:
+ *
+ * <pre class="code"><code class="java">
+ * <b>&#064;ExtendWith(MockBukkitExtension.class)</b>
+ * class MethodExampleTest
+ * {
+ *
+ * 	&#064;Test
+ * 	void aUnitTest(&#064;MockBukkitServer ServerMock serverMock)
+ *    {
+ * 		assert serverMock != null;
+ * 		// ...
+ *    }
  *
  * }
  * </code></pre>
  */
-@ApiStatus.Experimental
-public class MockBukkitExtension implements BeforeEachCallback, AfterEachCallback
+@Experimental
+public class MockBukkitExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver
 {
 
 	@Override
 	public void beforeEach(ExtensionContext context) throws Exception
 	{
-		MockBukkit.mock();
+		MockBukkit.getOrCreateMock();
 	}
 
 	@Override
 	public void afterEach(ExtensionContext context) throws Exception
 	{
 		MockBukkit.unmock();
+	}
+
+	@Override
+	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException
+	{
+		return parameterContext.isAnnotated(MockBukkitServer.class);
+	}
+
+	@Override
+	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException
+	{
+		final Parameter parameter = parameterContext.getParameter();
+		if (parameter.getType() != ServerMock.class)
+			return null;
+
+		// we only currently support ServerMock instances annotated with MockBukkitServer
+
+		final MockBukkitServer annotation = parameter.getAnnotation(MockBukkitServer.class);
+		if (annotation == null)
+			return null;
+
+		return MockBukkit.getOrCreateMock();
 	}
 
 }
---
+++ src/test/java/be/seeseemelk/mockbukkit/MockBukkitServerTest.java
@@ -0,0 +1,40 @@
+package be.seeseemelk.mockbukkit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockBukkitExtension.class)
+public class MockBukkitServerTest
+{
+
+//	@MockBukkitServer
+//	private ServerMock fieldServerMock;
+
+	private ServerMock constructorParameterServerMock;
+
+	public MockBukkitServerTest(@MockBukkitServer ServerMock serverMock)
+	{
+		this.constructorParameterServerMock = serverMock;
+	}
+
+//	@Test
+//	void mockBukkitServerAnnotation_InjectsField()
+//	{
+//		assertNotNull(fieldServerMock);
+//	}
+
+	@Test
+	void mockBukkitServerAnnotation_InjectsConstructorParameter()
+	{
+		assertNotNull(constructorParameterServerMock);
+	}
+
+	@Test
+	void mockBukkitServerAnnotation_InjectsMethodParameter(@MockBukkitServer ServerMock methodParameter)
+	{
+		assertNotNull(methodParameter);
+	}
+
+}
