9a4dc4f2bc7460fb1ae4c744a4fa8870ef197333
Baptiste <baptiste.janton@epitech.eu>
2022-11-02 11:25:32 -0400

    Remove public visibility of polar bear test class

--- src/test/java/be/seeseemelk/mockbukkit/entity/PolarBearMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/entity/PolarBearMockTest.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PolarBearMockTest
+class PolarBearMockTest
 {
 
 	private ServerMock server;
