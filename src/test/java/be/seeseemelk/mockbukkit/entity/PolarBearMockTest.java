6070f467a44ddabc11e4991e678dc16ed681ce2a
Baptiste <baptiste.janton@epitech.eu>
2022-11-02 11:09:40 -0400

    Implement Polar bear mock

---
+++ src/test/java/be/seeseemelk/mockbukkit/entity/PolarBearMockTest.java
@@ -0,0 +1,48 @@
+package be.seeseemelk.mockbukkit.entity;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PolarBearMockTest
+{
+
+	private ServerMock server;
+	private PolarBearMock polarBear;
+
+	@BeforeEach
+	void setUp()
+	{
+		server = MockBukkit.mock();
+		polarBear = new PolarBearMock(server, UUID.randomUUID());
+	}
+
+	@AfterEach
+	void tearDown()
+	{
+		MockBukkit.unmock();
+	}
+
+	@Test
+	void testIsStandingDefault() {
+		assertFalse(polarBear.isStanding());
+	}
+
+	@Test
+	void testSetStanding() {
+		polarBear.setStanding(true);
+		assertTrue(polarBear.isStanding());
+	}
+
+	@Test
+	void testCanBreed() {
+		assertFalse(polarBear.canBreed());
+	}
+}
