72dfdde82e6b75edad3cb715a9d714a62b31ae8a
Pierce Thompson <insprill@gmail.com>
2022-11-06 20:37:34 -0500

    Add a Javadoc for KeyedBossBarMock's constructor

--- src/main/java/be/seeseemelk/mockbukkit/boss/KeyedBossBarMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/boss/KeyedBossBarMock.java
@@ -15,6 +15,15 @@ public class KeyedBossBarMock extends BossBarMock implements KeyedBossBar
 
 	private final @NotNull NamespacedKey key;
 
+	/**
+	 * Constructs a new {@link KeyedBossBarMock} with the provided parameters.
+	 *
+	 * @param key   The bossbar's namespaced key.
+	 * @param title The title of the bossbar.
+	 * @param color The color of the bossbar.
+	 * @param style The style of the bossbar.
+	 * @param flags The flags to set on the bossbar.
+	 */
 	public KeyedBossBarMock(@NotNull NamespacedKey key, @NotNull String title, @NotNull BarColor color, @NotNull BarStyle style, BarFlag... flags)
 	{
 		super(title, color, style, flags);
