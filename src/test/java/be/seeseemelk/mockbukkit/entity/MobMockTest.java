ef015d9dea1ff8e7c47ea27cf4c3e0f4297934cb
Lena Kolb <lenakolb2204@gmail.com>
2022-12-02 00:21:20 +0100

    Remove Public Modifier

--- src/test/java/be/seeseemelk/mockbukkit/entity/MobMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/entity/MobMockTest.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class MobMockTest
+class MobMockTest
 {
 
 	private MobMock mob;
