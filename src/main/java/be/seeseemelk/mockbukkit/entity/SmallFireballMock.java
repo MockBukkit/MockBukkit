c6c0984a03086ff889f1bb8d31c2804d130c8f7a
Lena Kolb <lenakolb2204@gmail.com>
2022-12-08 00:44:11 +0100

    Implement Fireballs

---
+++ src/main/java/be/seeseemelk/mockbukkit/entity/SmallFireballMock.java
@@ -0,0 +1,30 @@
+package be.seeseemelk.mockbukkit.entity;
+
+import be.seeseemelk.mockbukkit.ServerMock;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.SmallFireball;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class SmallFireballMock extends SizedFireballMock implements SmallFireball
+{
+
+	/**
+	 * Constructs a new {@link ProjectileMock} on the provided {@link ServerMock} with a specified {@link UUID}.
+	 *
+	 * @param server The server to create the entity on.
+	 * @param uuid   The UUID of the entity.
+	 */
+	public SmallFireballMock(@NotNull ServerMock server, @NotNull UUID uuid)
+	{
+		super(server, uuid);
+	}
+
+	@Override
+	public @NotNull EntityType getType()
+	{
+		return EntityType.SMALL_FIREBALL;
+	}
+
+}
