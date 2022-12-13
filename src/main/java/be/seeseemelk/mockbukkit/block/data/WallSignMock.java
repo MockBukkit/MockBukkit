6a29e41f81a1249a5202cd35559dec1240872214
Thorinwasher <ragnarlol96@gmail.com>
2022-12-13 17:59:37 +0100

    replaces spaces with tabs

--- src/main/java/be/seeseemelk/mockbukkit/block/data/WallSignMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/block/data/WallSignMock.java
@@ -11,47 +11,46 @@ import org.jetbrains.annotations.NotNull;
 import static be.seeseemelk.mockbukkit.block.data.BlockDataKey.FACING;
 import static be.seeseemelk.mockbukkit.block.data.BlockDataKey.WATERLOGGED;
 
-public class WallSignMock extends BlockDataMock implements WallSign{
-    
-    /**
-     * Constructs a new {@link WallSignMock} for the provided {@link Material}.
-     * Only supports materials in {@link Tag#WALL_SIGNS}
-     * 
-     * @param type The material this data is for.
-     */
-    WallSignMock(@NotNull Material type) {
-        super(type);
-        checkType(type, Tag.WALL_SIGNS);
-        this.setFacing(BlockFace.NORTH);
-        this.setWaterlogged(false);
-    }
+public class WallSignMock extends BlockDataMock implements WallSign {
 
-    @Override
-    public @NotNull BlockFace getFacing() {
-        return super.get(FACING);
-    }
+	/**
+	 * Constructs a new {@link WallSignMock} for the provided {@link Material}. Only
+	 * supports materials in {@link Tag#WALL_SIGNS}
+	 * 
+	 * @param type The material this data is for.
+	 */
+	WallSignMock(@NotNull Material type) {
+		super(type);
+		checkType(type, Tag.WALL_SIGNS);
+		this.setFacing(BlockFace.NORTH);
+		this.setWaterlogged(false);
+	}
 
-    @Override
-    public void setFacing(@NotNull BlockFace facing) {
-        if (!getFaces().contains(facing))
-        {
-            throw new IllegalArgumentException("Invalid face: " + facing);
-        }
-        super.set(FACING, facing);
-    }
+	@Override
+	public @NotNull BlockFace getFacing() {
+		return super.get(FACING);
+	}
 
-    @Override
-    public @NotNull Set<BlockFace> getFaces() {
-        return Set.of(BlockFace.EAST, BlockFace.NORTH,BlockFace.WEST, BlockFace.SOUTH);
-    }
+	@Override
+	public void setFacing(@NotNull BlockFace facing) {
+		if (!getFaces().contains(facing)) {
+			throw new IllegalArgumentException("Invalid face: " + facing);
+		}
+		super.set(FACING, facing);
+	}
 
-    @Override
-    public boolean isWaterlogged() {
-        return super.get(WATERLOGGED);
-    }
+	@Override
+	public @NotNull Set<BlockFace> getFaces() {
+		return Set.of(BlockFace.EAST, BlockFace.NORTH, BlockFace.WEST, BlockFace.SOUTH);
+	}
 
-    @Override
-    public void setWaterlogged(boolean waterlogged) {
-        super.set(WATERLOGGED, waterlogged);
-    }
+	@Override
+	public boolean isWaterlogged() {
+		return super.get(WATERLOGGED);
+	}
+
+	@Override
+	public void setWaterlogged(boolean waterlogged) {
+		super.set(WATERLOGGED, waterlogged);
+	}
 }
--- src/test/java/be/seeseemelk/mockbukkit/block/data/WallSignMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/block/data/WallSignMockTest.java
@@ -23,129 +23,119 @@ import be.seeseemelk.mockbukkit.WorldMock;
 
 class WallSignMockTest {
 
-    private WallSignMock sign;
-    private @NotNull ServerMock server;
+	private WallSignMock sign;
+	private @NotNull ServerMock server;
 
-    @BeforeEach
-    void setUp() throws IOException {
-        MockBukkit.mock();
-        sign = new WallSignMock(Material.ACACIA_WALL_SIGN);
-    }
-    
-    @AfterEach
-    void tearDown() {
-        MockBukkit.unmock();
-    }
-    
-    
-    @Test
-    @Order(1)
-    void constructor_FullConstruction() {
-        WorldMock world = new WorldMock();
-        Block signBlock = world.getBlockAt(new Location(world,0,2,1));
-        signBlock.setType(Material.OAK_WALL_SIGN);
+	@BeforeEach
+	void setUp() throws IOException {
+		MockBukkit.mock();
+		sign = new WallSignMock(Material.ACACIA_WALL_SIGN);
+	}
 
-        Assertions.assertEquals(WallSignMock.class,signBlock.getBlockData().getClass());
-    }
-    
-    
-    @Test
-    @Order(2)
-    void constructor_DefaultValues() {
-        assertEquals(BlockFace.NORTH, sign.getFacing());
-        assertEquals(false,sign.isWaterlogged());
-    }
-    
-    @Test
-    @Order(2)
-    void constructor_Material() {
-        for(Material wallSignType : Tag.WALL_SIGNS.getValues()) {
-            assertDoesNotThrow(() -> new WallSignMock(wallSignType));
-        }
-    }
-    
-    @Test
-    @Order(2)
-    void constructor_Material_WrongType_ThrowsException()
-    {
-        assertThrowsExactly(IllegalArgumentException.class, () -> new WallSignMock(Material.BEDROCK));
-    }
-    
-    @Test
-    @Order(2)
-    void setFacing_Valid()
-    {
-        for (BlockFace face : BlockFace.values())
-        {
-            if (!sign.getFaces().contains(face))
-                continue;
-            assertDoesNotThrow(() -> sign.setFacing(face));
-            assertEquals(face,sign.getFacing());
-        }
-    }
-    
-    @Test
-    @Order(2)
-    void setFacing_Invalid()
-    {
-        for (BlockFace face : BlockFace.values())
-        {
-            if (sign.getFaces().contains(face))
-                continue;
-            assertThrowsExactly(IllegalArgumentException.class, () -> sign.setFacing(face));
-        }
-    }
-    
-    @Test
-    @Order(2)
-    void getFaces()
-    {
-        Set<BlockFace> validFaces = Set.of(BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST);
-        assertEquals(validFaces, sign.getFaces());
-    }
-    
-    @Test
-    @Order(2)
-    void getFacing_Immutable()
-    {
-        Set<BlockFace> faces = sign.getFaces();
-        assertThrows(UnsupportedOperationException.class, () -> faces.add(BlockFace.NORTH_EAST));
-    }
-    
-    @Test 
-    @Order(2)
-    void setWaterLogged(){
-        assertDoesNotThrow(() -> sign.setWaterlogged(true));
-        assertEquals(true,sign.isWaterlogged());
-        assertDoesNotThrow(() -> sign.setWaterlogged(false));
-        assertEquals(false,sign.isWaterlogged());
-    }
-    
-    @Test
-    @Order(2)
-    void getAsString()
-    {   
-        sign.setWaterlogged(true);
-        for(BlockFace facing : sign.getFaces()) {
-            String testString = String.format("minecraft:acacia_wall_sign[facing=%s,waterlogged=%s]",facing.toString().toLowerCase(),true);
-            sign.setFacing(facing);
-            assertEquals(testString,sign.getAsString());
-        }
-        sign.setWaterlogged(false);
-        for(BlockFace facing : sign.getFaces()) {
-            String testString = String.format("minecraft:acacia_wall_sign[facing=%s,waterlogged=%s]",facing.toString().toLowerCase(),false);
-            sign.setFacing(facing);
-            assertEquals(testString,sign.getAsString());
-        }
-    }
-    
-    @Test
-    @Order(2)
-    void blockDataMock_Mock_CorrectType()
-    {
-        for (Material material : Tag.WALL_SIGNS.getValues())
-        {
-            assertInstanceOf(WallSignMock.class, BlockDataMock.mock(material));
-        }
-    }
+	@AfterEach
+	void tearDown() {
+		MockBukkit.unmock();
+	}
+
+	@Test
+	@Order(1)
+	void constructor_FullConstruction() {
+		WorldMock world = new WorldMock();
+		Block signBlock = world.getBlockAt(new Location(world, 0, 2, 1));
+		signBlock.setType(Material.OAK_WALL_SIGN);
+
+		Assertions.assertEquals(WallSignMock.class, signBlock.getBlockData().getClass());
+	}
+
+	@Test
+	@Order(2)
+	void constructor_DefaultValues() {
+		assertEquals(BlockFace.NORTH, sign.getFacing());
+		assertEquals(false, sign.isWaterlogged());
+	}
+
+	@Test
+	@Order(2)
+	void constructor_Material() {
+		for (Material wallSignType : Tag.WALL_SIGNS.getValues()) {
+			assertDoesNotThrow(() -> new WallSignMock(wallSignType));
+		}
+	}
+
+	@Test
+	@Order(2)
+	void constructor_Material_WrongType_ThrowsException() {
+		assertThrowsExactly(IllegalArgumentException.class, () -> new WallSignMock(Material.BEDROCK));
+	}
+
+	@Test
+	@Order(2)
+	void setFacing_Valid() {
+		for (BlockFace face : BlockFace.values()) {
+			if (!sign.getFaces().contains(face))
+				continue;
+			assertDoesNotThrow(() -> sign.setFacing(face));
+			assertEquals(face, sign.getFacing());
+		}
+	}
+
+	@Test
+	@Order(2)
+	void setFacing_Invalid() {
+		for (BlockFace face : BlockFace.values()) {
+			if (sign.getFaces().contains(face))
+				continue;
+			assertThrowsExactly(IllegalArgumentException.class, () -> sign.setFacing(face));
+		}
+	}
+
+	@Test
+	@Order(2)
+	void getFaces() {
+		Set<BlockFace> validFaces = Set.of(BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST);
+		assertEquals(validFaces, sign.getFaces());
+	}
+
+	@Test
+	@Order(2)
+	void getFacing_Immutable() {
+		Set<BlockFace> faces = sign.getFaces();
+		assertThrows(UnsupportedOperationException.class, () -> faces.add(BlockFace.NORTH_EAST));
+	}
+
+	@Test
+	@Order(2)
+	void setWaterLogged() {
+		assertDoesNotThrow(() -> sign.setWaterlogged(true));
+		assertEquals(true, sign.isWaterlogged());
+		assertDoesNotThrow(() -> sign.setWaterlogged(false));
+		assertEquals(false, sign.isWaterlogged());
+	}
+
+	@Test
+	@Order(2)
+	void getAsString() {
+		sign.setWaterlogged(true);
+		for (BlockFace facing : sign.getFaces()) {
+			String testString = String.format("minecraft:acacia_wall_sign[facing=%s,waterlogged=%s]",
+					facing.toString().toLowerCase(), true);
+			sign.setFacing(facing);
+			assertEquals(testString, sign.getAsString());
+		}
+		sign.setWaterlogged(false);
+		for (BlockFace facing : sign.getFaces()) {
+			String testString = String.format("minecraft:acacia_wall_sign[facing=%s,waterlogged=%s]",
+					facing.toString().toLowerCase(), false);
+			sign.setFacing(facing);
+			assertEquals(testString, sign.getAsString());
+		}
+	}
+
+	@Test
+	@Order(2)
+	void blockDataMock_Mock_CorrectType() {
+		for (Material material : Tag.WALL_SIGNS.getValues()) {
+			assertInstanceOf(WallSignMock.class, BlockDataMock.mock(material));
+		}
+	}
 }
