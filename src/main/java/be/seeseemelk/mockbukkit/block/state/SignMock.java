1385c5c128541f50bd013ee30509bcb27bf2006b
Thorinwasher <37477983+Thorinwasher@users.noreply.github.com>
2022-12-15 17:24:01 +0100

    Update src/main/java/be/seeseemelk/mockbukkit/block/state/SignMock.java
    Co-authored-by: Pierce Thompson <52843537+Insprill@users.noreply.github.com>

--- src/main/java/be/seeseemelk/mockbukkit/block/state/SignMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/block/state/SignMock.java
@@ -64,8 +64,8 @@ public class SignMock extends TileStateMock implements Sign
 		{
 			lines[i] = state.getLine(i);
 		}
-		color = state.getColor();
-		glowing = state.isGlowingText();
+		this.color = state.getColor();
+		this.glowing = state.isGlowingText();
 	}
 
 	@Override
