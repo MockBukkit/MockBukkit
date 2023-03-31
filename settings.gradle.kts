6c64cce0b5e19d1725e3fc72deade10561b048fa
Pierce Thompson <insprill@gmail.com>
2023-03-31 00:54:29 -0400

    Dynamically set project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@
   This is the name of our MockBukkit artifact, it includes
   the API version of Minecraft we are targeting.
 */
-rootProject.name = "MockBukkit-1.19"
+rootProject.name = "MockBukkit-${extensions.extraProperties.get("paper.api.version")}"
