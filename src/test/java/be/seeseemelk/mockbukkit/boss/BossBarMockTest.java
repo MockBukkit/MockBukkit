ba05bb4b220b14a3a82d311d16670d56c49d4f9e
thelooter <lenakolb2204@gmail.com>
2023-04-21 22:55:06 +0200

    Remove unused Import

--- src/test/java/be/seeseemelk/mockbukkit/boss/BossBarMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/boss/BossBarMockTest.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
