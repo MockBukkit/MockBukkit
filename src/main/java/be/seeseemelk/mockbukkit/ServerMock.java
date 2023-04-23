c8478bec0f795d527cfced6d96f38228aa49e8d7
Janet Blackquill <uhhadd@gmail.com>
2023-04-23 13:32:50 -0400

    Fix loading of .properties files from MockBukkit resources

--- src/main/java/be/seeseemelk/mockbukkit/ServerMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/ServerMock.java
@@ -205,7 +205,7 @@ public class ServerMock extends Server.Spigot implements Server
 
 		try
 		{
-			InputStream stream = ClassLoader.getSystemResourceAsStream("logger.properties");
+			InputStream stream = getClass().getClassLoader().getResourceAsStream("logger.properties");
 			LogManager.getLogManager().readConfiguration(stream);
 		}
 		catch (IOException e)
@@ -216,7 +216,7 @@ public class ServerMock extends Server.Spigot implements Server
 
 		try
 		{
-			buildProperties.load(ClassLoader.getSystemResourceAsStream("build.properties"));
+			buildProperties.load(getClass().getClassLoader().getResourceAsStream("build.properties"));
 		}
 		catch (IOException | NullPointerException e)
 		{
