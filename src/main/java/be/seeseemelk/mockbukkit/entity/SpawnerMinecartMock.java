6c7ed75fce2124be3f0e67fffa32967903299bbd
thelooter <lenakolb2204@gmail.com>
2023-05-24 16:54:01 +0200

    Add Spawner Minecart

---
+++ src/main/java/be/seeseemelk/mockbukkit/entity/SpawnerMinecartMock.java
@@ -0,0 +1,30 @@
+package be.seeseemelk.mockbukkit.entity;
+
+import be.seeseemelk.mockbukkit.ServerMock;
+import org.bukkit.Material;
+import org.bukkit.entity.minecart.SpawnerMinecart;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class SpawnerMinecartMock extends MinecartMock implements SpawnerMinecart
+{
+
+	/**
+	 * Constructs a new {@link SpawnerMinecartMock} on the provided {@link ServerMock} with a specified {@link UUID}.
+	 *
+	 * @param server The server to create the entity on.
+	 * @param uuid   The UUID of the entity.
+	 */
+	public SpawnerMinecartMock(@NotNull ServerMock server, @NotNull UUID uuid)
+	{
+		super(server, uuid);
+	}
+
+	@Override
+	public @NotNull Material getMinecartMaterial()
+	{
+		return Material.MINECART;
+	}
+
+}
---
+++ src/test/java/be/seeseemelk/mockbukkit/entity/SpawnerMinecartMockTest.java
@@ -0,0 +1,35 @@
+package be.seeseemelk.mockbukkit.entity;
+
+import be.seeseemelk.mockbukkit.MockBukkitExtension;
+import be.seeseemelk.mockbukkit.MockBukkitInject;
+import be.seeseemelk.mockbukkit.ServerMock;
+import org.bukkit.Material;
+import org.bukkit.entity.minecart.SpawnerMinecart;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockBukkitExtension.class)
+class SpawnerMinecartMockTest
+{
+
+	@MockBukkitInject
+	private ServerMock server;
+	private SpawnerMinecart minecart;
+
+	@BeforeEach
+	public void setUp() throws Exception
+	{
+		minecart = new SpawnerMinecartMock(server, UUID.randomUUID());
+	}
+
+	@Test
+	void testGetMinecartMaterial()
+	{
+		assertEquals(minecart.getMinecartMaterial(), Material.MINECART);
+	}
+}
