7e10ee5ee2034694004190cbae15cfe5515a0911
thelooter <lenakolb2204@gmail.com>
2023-05-27 01:48:33 +0200

    Adds Hamcrest Dependency and creates an AbstractMatcherTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("net.kyori:adventure-platform-bungeecord:4.3.0")
 	implementation("org.jetbrains:annotations:24.0.1")
 	implementation("net.bytebuddy:byte-buddy:1.14.4")
+	implementation("org.hamcrest:hamcrest:2.2")
 
 	// LibraryLoader dependencies
 	implementation("org.apache.maven:maven-resolver-provider:3.8.5")
