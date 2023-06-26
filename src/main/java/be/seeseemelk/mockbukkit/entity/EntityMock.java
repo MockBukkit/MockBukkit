3bf7891bf367932809e9bf718091a8d2f252a96d
Gabber235 <gabber200@icloud.com>
2023-06-26 08:34:29 +0200

    Add Adventure BossBar viewing

--- src/main/java/be/seeseemelk/mockbukkit/entity/EntityMock.java
+++ src/main/java/be/seeseemelk/mockbukkit/entity/EntityMock.java
@@ -15,6 +15,7 @@ import com.google.common.base.Preconditions;
 import io.papermc.paper.entity.TeleportFlag;
 import io.papermc.paper.threadedregions.scheduler.EntityScheduler;
 import net.kyori.adventure.audience.MessageType;
+import net.kyori.adventure.bossbar.BossBar;
 import net.kyori.adventure.identity.Identity;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.HoverEvent;
@@ -104,6 +105,8 @@ public abstract class EntityMock extends Entity.Spigot implements Entity, Messag
 	private boolean visualFire;
 	private boolean silent;
 	private boolean gravity = true;
+
+	private final List<BossBar> bossBars = new ArrayList<>();
 
 	private final EntityData entityData;
 
@@ -1063,6 +1066,29 @@ public abstract class EntityMock extends Entity.Spigot implements Entity, Messag
 	public void setGravity(boolean gravity)
 	{
 		this.gravity = gravity;
+	}
+
+	@Override
+	public void showBossBar(@NotNull BossBar bar)
+	{
+		this.bossBars.add(bar);
+	}
+
+	@Override
+	public void hideBossBar(@NotNull BossBar bar)
+	{
+		this.bossBars.remove(bar);
+	}
+
+	/**
+	 * Gets the boss bars that are currently visible to this entity.
+	 *
+	 * @return A set of boss bars that are visible to this entity.
+	 */
+	@NotNull
+	public List<BossBar> getBossBars()
+	{
+		return this.bossBars;
 	}
 
 	@Override
