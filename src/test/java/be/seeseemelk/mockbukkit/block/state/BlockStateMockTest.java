f25c5f248eabd4e74880298556b73425fc569364
Thorinwasher <ragnarlol96@gmail.com>
2023-10-11 21:04:56 +0200

    Change BlockState equals and hasCode

--- src/test/java/be/seeseemelk/mockbukkit/block/state/BlockStateMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/block/state/BlockStateMockTest.java
@@ -6,23 +6,19 @@ import be.seeseemelk.mockbukkit.block.BlockMock;
 import be.seeseemelk.mockbukkit.block.data.BlockDataMock;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.TrapDoor;
+import org.bukkit.block.data.type.WallSign;
+import org.bukkit.material.MaterialData;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BlockStateMockTest
 {
@@ -130,7 +126,8 @@ class BlockStateMockTest
 	}
 
 	@Test
-	void clone_copyBlockData(){
+	void clone_copyBlockData()
+	{
 		Block block = new BlockMock(Material.CHEST);
 		BlockStateMock state = new ChestMock(block);
 		BlockState stateCopy = state.getSnapshot();
@@ -139,7 +136,8 @@ class BlockStateMockTest
 	}
 
 	@Test
-	void setBlockData_assertClone(){
+	void setBlockData_assertClone()
+	{
 		Block block = new BlockMock(Material.CHEST);
 		BlockStateMock state = new ChestMock(block);
 		BlockDataMock data = BlockDataMock.mock(Material.CHEST);
@@ -148,4 +146,53 @@ class BlockStateMockTest
 		assertNotSame(data, dataCopy);
 		assertEquals(data, dataCopy);
 	}
+
+	@Test
+	void equals_differentLocation()
+	{
+		World world = MockBukkit.getMock().addSimpleWorld("world");
+		BlockState blockState1 = new BlockMock(Material.STONE, new Location(world, 1, 2, 3)).getState();
+		BlockState blockState2 = new BlockMock(Material.STONE).getState();
+		assertNotEquals(blockState1, blockState2);
+	}
+
+	@Test
+	void equals_differentMaterial()
+	{
+		BlockStateMock blockState1 = (BlockStateMock) new BlockMock(Material.STONE).getState();
+		BlockState blockState2 = blockState1.getSnapshot();
+		assertEquals(blockState1, blockState2);
+		blockState2.setType(Material.AIR);
+		assertNotEquals(blockState1, blockState2);
+	}
+
+	@Test
+	void equals_differentBlockData()
+	{
+		BlockStateMock blockState1 = (BlockStateMock) new BlockMock(Material.ACACIA_WALL_SIGN).getState();
+		BlockState blockState2 = blockState1.getSnapshot();
+		WallSign wallSign = (WallSign) BlockDataMock.mock(Material.ACACIA_WALL_SIGN);
+		blockState2.setBlockData(wallSign);
+		assertEquals(blockState1, blockState2);
+		wallSign.setWaterlogged(true);
+		blockState2.setBlockData(wallSign);
+		assertNotEquals(blockState1, blockState2);
+	}
+
+	@Test
+	void setType_checkBlockData()
+	{
+		BlockStateMock blockState1 = (BlockStateMock) new BlockMock(Material.ACACIA_WALL_SIGN).getState();
+		blockState1.setType(Material.OAK_WALL_SIGN);
+		assertEquals(Material.OAK_WALL_SIGN, blockState1.getBlockData().getMaterial());
+	}
+
+	@Test
+	void setData_checkBlockData()
+	{
+		BlockStateMock blockState1 = (BlockStateMock) new BlockMock(Material.ACACIA_WALL_SIGN).getState();
+		blockState1.setData(new MaterialData(Material.OAK_WALL_SIGN));
+		assertEquals(Material.OAK_WALL_SIGN, blockState1.getBlockData().getMaterial());
+	}
+
 }
