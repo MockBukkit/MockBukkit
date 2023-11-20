4fe4dfed61bd47ef82e57af9540a4effd4a0b934
thelooter <evekolb2204@gmail.com>
2023-11-20 21:19:07 +0100

    feat(matcher): Add Annotationa

--- src/main/java/org/mockbukkit/mockbukkit/matcher/allay/AllayInventoryContainsMatcher.java
+++ src/main/java/org/mockbukkit/mockbukkit/matcher/allay/AllayInventoryContainsMatcher.java
@@ -3,6 +3,8 @@ package org.mockbukkit.mockbukkit.matcher.allay;
 import org.bukkit.inventory.ItemStack;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.mockbukkit.mockbukkit.entity.AllayMock;
 
 public class AllayInventoryContainsMatcher extends TypeSafeMatcher<AllayMock>
@@ -16,24 +18,25 @@ public class AllayInventoryContainsMatcher extends TypeSafeMatcher<AllayMock>
 	}
 
 	@Override
-	protected boolean matchesSafely(AllayMock allay)
+	protected boolean matchesSafely(@NotNull AllayMock allay)
 	{
 		return allay.getInventory().contains(itemStack);
 	}
 
 	@Override
-	public void describeTo(Description description)
+	public void describeTo(@NotNull Description description)
 	{
 		description.appendText("Should have Itemstack in inventory");
 	}
 
 	@Override
-	protected void describeMismatchSafely(AllayMock item, Description mismatchDescription)
+	protected void describeMismatchSafely(AllayMock item, @NotNull Description mismatchDescription)
 	{
 		mismatchDescription.appendText("doesn't have Itemstack \"" + itemStack.toString() + "\" in Inventory");
 	}
 
-	public static AllayInventoryContainsMatcher inventoryContains(ItemStack itemStack)
+	@Contract("_ -> new")
+	public static @NotNull AllayInventoryContainsMatcher inventoryContains(ItemStack itemStack)
 	{
 		return new AllayInventoryContainsMatcher(itemStack);
 	}
