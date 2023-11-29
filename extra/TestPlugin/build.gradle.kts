0680b4701d893b198a1714c364d828d695b04afe
Nirbose <nirbose2@gmail.com>
2023-11-30 00:01:09 +0100

    fix test

--- extra/TestPlugin/build.gradle.kts
+++ extra/TestPlugin/build.gradle.kts
@@ -9,3 +9,7 @@ repositories {
 dependencies {
 	compileOnly("io.papermc.paper:paper-api:${rootProject.property("paper.api.full-version")}")
 }
+java {
+    sourceCompatibility = JavaVersion.VERSION_19
+    targetCompatibility = JavaVersion.VERSION_19
+}
