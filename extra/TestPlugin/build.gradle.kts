32138da521d7be62c21d144e6a3e30ea86784d0e
Nirbose <nirbose2@gmail.com>
2023-11-30 00:03:26 +0100

    remove javaVersion build.gradle.kts

--- extra/TestPlugin/build.gradle.kts
+++ extra/TestPlugin/build.gradle.kts
@@ -9,7 +9,3 @@ repositories {
 dependencies {
 	compileOnly("io.papermc.paper:paper-api:${rootProject.property("paper.api.full-version")}")
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_19
-    targetCompatibility = JavaVersion.VERSION_19
-}
