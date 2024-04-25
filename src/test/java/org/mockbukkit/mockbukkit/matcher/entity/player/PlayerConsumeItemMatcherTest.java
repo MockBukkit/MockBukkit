ecdb096c8f765e8696cfe6fda0f96f3a30172986
Thorinwasher <ragnarlol96@gmail.com>
2024-04-25 15:24:23 +0200

    Hamcrest: implement player consume item matcher

---
+++ src/test/java/org/mockbukkit/mockbukkit/matcher/entity/player/PlayerConsumeItemMatcherTest.java
@@ -0,0 +1,66 @@
+package org.mockbukkit.mockbukkit.matcher.entity.player;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockbukkit.mockbukkit.MockBukkitExtension;
+import org.mockbukkit.mockbukkit.MockBukkitInject;
+import org.mockbukkit.mockbukkit.ServerMock;
+import org.mockbukkit.mockbukkit.entity.PlayerMock;
+import org.mockbukkit.mockbukkit.simulate.entity.PlayerSimulation;
+import org.mockbukkit.testutils.matcher.AbstractMatcherTest;
+
+import static org.mockbukkit.mockbukkit.matcher.entity.player.PlayerConsumeItemMatcher.hasConsumed;
+
+@ExtendWith(MockBukkitExtension.class)
+class PlayerConsumeItemMatcherTest extends AbstractMatcherTest
+{
+
+	@MockBukkitInject
+	ServerMock serverMock;
+	private PlayerMock player;
+	private ItemStack item;
+
+	@BeforeEach
+	void setUp()
+	{
+		this.player = serverMock.addPlayer();
+		this.item = new ItemStack(Material.POTATO);
+	}
+
+	@Test
+	void consumed_matches()
+	{
+		new PlayerSimulation(player).simulateConsumeItem(item);
+		assertMatches(hasConsumed(item), player);
+	}
+
+	@Test
+	void consumed_differentItem()
+	{
+		new PlayerSimulation(player).simulateConsumeItem(new ItemStack(Material.APPLE));
+		assertDoesNotMatch(hasConsumed(item), player);
+	}
+
+	@Test
+	void consumed_noItem()
+	{
+		assertDoesNotMatch(hasConsumed(item), player);
+	}
+
+	@Test
+	void isNullSafe()
+	{
+		testIsNullSafe();
+	}
+
+	@Override
+	protected Matcher<?> createMatcher()
+	{
+		return hasConsumed(item);
+	}
+
+}
