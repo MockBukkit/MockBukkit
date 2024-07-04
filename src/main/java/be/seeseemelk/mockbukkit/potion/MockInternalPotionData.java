878c457f488220809d0c3874642e003bbc989c2d
Felix Naumann <wand516@gmail.com>
2024-07-04 23:36:25 +0200

    fire assertEntityPotionEffectEvent when a potion effect is added

--- src/main/java/be/seeseemelk/mockbukkit/potion/MockInternalPotionData.java
+++ src/main/java/be/seeseemelk/mockbukkit/potion/MockInternalPotionData.java
@@ -19,7 +19,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-
 public class MockInternalPotionData implements PotionType.InternalPotionData
 {
 
