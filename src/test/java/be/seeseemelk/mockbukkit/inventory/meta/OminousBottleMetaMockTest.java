618dcfda1cf272c43828a1c4c05db8d41557990a
thelooter <evekolb2204@gmail.com>
2024-07-07 17:11:37 +0200

    chore(quality): Fix some more codesmells

--- src/test/java/be/seeseemelk/mockbukkit/inventory/meta/OminousBottleMetaMockTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/inventory/meta/OminousBottleMetaMockTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockBukkitExtension.class)
-public class OminousBottleMetaMockTest
+class OminousBottleMetaMockTest
 {
 
 	@MockBukkitInject
