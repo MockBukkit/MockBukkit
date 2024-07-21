14f81684718216b059a3386bc453cd5502ba005f
Thorinwasher <ragnarlol96@gmail.com>
2024-07-21 20:09:42 +0200

    Add capability to scan methods for keyed instance generation

--- src/main/java/org/mockbukkit/metaminer/keyed/KeyedClassTracker.java
+++ src/main/java/org/mockbukkit/metaminer/keyed/KeyedClassTracker.java
@@ -2,6 +2,7 @@ package org.mockbukkit.metaminer.keyed;
 
 import io.leangen.geantyref.GenericTypeReflector;
 import io.papermc.paper.registry.RegistryKey;
+import org.bukkit.Keyed;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
@@ -12,11 +13,11 @@ import java.util.Map;
 public class KeyedClassTracker
 {
 
-	public static Map<RegistryKey<?>, Class<?>> CLASS_REGISTRY_KEY_RELATION = loadClassRegistryKeyRelation();
+	public static Map<RegistryKey<? extends Keyed>, Class<?>> CLASS_REGISTRY_KEY_RELATION = loadClassRegistryKeyRelation();
 
-	private static Map<RegistryKey<?>, Class<?>> loadClassRegistryKeyRelation()
+	private static Map<RegistryKey<? extends Keyed>, Class<?>> loadClassRegistryKeyRelation()
 	{
-		Map<RegistryKey<?>, Class<?>> output = new HashMap<>();
+		Map<RegistryKey<? extends Keyed>, Class<?>> output = new HashMap<>();
 		for (final Field field : RegistryKey.class.getFields())
 		{
 			if (field.getType() == RegistryKey.class)
@@ -25,7 +26,7 @@ public class KeyedClassTracker
 				final Class<?> legacyType = GenericTypeReflector.erase(((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0]);
 				try
 				{
-					RegistryKey<?> registryKey = (RegistryKey<?>) field.get(null);
+					RegistryKey<? extends Keyed> registryKey = (RegistryKey<? extends Keyed>) field.get(null);
 					output.put(registryKey, legacyType);
 				}
 				catch (IllegalAccessException e)
--- src/main/java/org/mockbukkit/metaminer/keyed/RegistryKeyClassDataGenerator.java
+++ src/main/java/org/mockbukkit/metaminer/keyed/RegistryKeyClassDataGenerator.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import io.papermc.paper.registry.RegistryKey;
+import org.bukkit.Keyed;
 import org.mockbukkit.metaminer.DataGenerator;
 import org.mockbukkit.metaminer.util.JsonUtil;
 
@@ -30,7 +31,7 @@ public class RegistryKeyClassDataGenerator implements DataGenerator
 		File destinationFile = new File(dataFolder, "registry_key_class_relation.json");
 		JsonObject rootObject = new JsonObject();
 
-		for (Map.Entry<RegistryKey<?>, Class<?>> entry : KeyedClassTracker.CLASS_REGISTRY_KEY_RELATION.entrySet())
+		for (Map.Entry<RegistryKey<? extends Keyed>, Class<?>> entry : KeyedClassTracker.CLASS_REGISTRY_KEY_RELATION.entrySet())
 		{
 			rootObject.add(entry.getKey().key().asString(), new JsonPrimitive(entry.getValue().getName()));
 		}
