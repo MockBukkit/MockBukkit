199e4951e7210314b7c64bbaabb4ca55b02bdcd8
ybw0014 <i@guizhanss.com>
2024-08-08 18:03:55 -0400

    chore: use default test class visibility

--- src/test/java/be/seeseemelk/mockbukkit/potion/PotionUtilsTest.java
+++ src/test/java/be/seeseemelk/mockbukkit/potion/PotionUtilsTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PotionUtilsTest
+class PotionUtilsTest
 {
 
 	@BeforeEach
