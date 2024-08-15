2b203726f7e481140667c9dfa5bd717fa2be9244
4everTheOne <5434488+4everTheOne@users.noreply.github.com>
2024-08-15 10:55:36 +0100

    Added logic for PillagerMock

---
+++ src/main/java/be/seeseemelk/mockbukkit/entity/PillagerMock.java
@@ -0,0 +1,46 @@
+package be.seeseemelk.mockbukkit.entity;
+
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.inventory.InventoryMock;
+import org.bukkit.Sound;
+import org.bukkit.entity.Pillager;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+/**
+ * Mock implementation of a {@link Pillager}.
+ *
+ * @see IllagerMock
+ */
+public class PillagerMock extends IllagerMock implements Pillager, MockRangedEntity<PillagerMock>
+{
+
+	private final Inventory inventory = new InventoryMock(this, 5, InventoryType.CHEST);
+
+	/**
+	 * Constructs a new {@link RaiderMock} on the provided {@link ServerMock} with a specified {@link UUID}.
+	 *
+	 * @param server The server to create the entity on.
+	 * @param uuid   The UUID of the entity.
+	 */
+	protected PillagerMock(@NotNull ServerMock server, @NotNull UUID uuid)
+	{
+		super(server, uuid);
+	}
+
+	@Override
+	public @NotNull Sound getCelebrationSound()
+	{
+		return Sound.ENTITY_PILLAGER_CELEBRATE;
+	}
+
+	@Override
+	public @NotNull Inventory getInventory()
+	{
+		return this.inventory;
+	}
+
+}
---
+++ src/test/java/be/seeseemelk/mockbukkit/entity/PillagerMockTest.java
@@ -0,0 +1,52 @@
+package be.seeseemelk.mockbukkit.entity;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import org.bukkit.Sound;
+import org.bukkit.inventory.Inventory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class PillagerMockTest
+{
+
+	private ServerMock server;
+	private PillagerMock pillager;
+
+	@BeforeEach
+	void setUp()
+	{
+		server = MockBukkit.mock();
+		pillager = new PillagerMock(server, UUID.randomUUID());
+	}
+
+	@AfterEach
+	void tearDown()
+	{
+		MockBukkit.unmock();
+	}
+
+	@Test
+	void getCelebrationSound()
+	{
+		assertEquals(Sound.ENTITY_PILLAGER_CELEBRATE, pillager.getCelebrationSound());
+	}
+
+	@Test
+	void getInventory()
+	{
+		Inventory inventory = pillager.getInventory();
+
+		assertNotNull(inventory);
+		assertEquals(5, inventory.getSize());
+		assertSame(inventory, pillager.getInventory());
+	}
+
+}
