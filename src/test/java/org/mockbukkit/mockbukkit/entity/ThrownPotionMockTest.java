c68f7ecddf3bea627834891c2e6a227a74b28f14
alexcrea <42614139+alexcrea@users.noreply.github.com>
2024-11-12 14:24:36 +0100

    Make non potion material fail is valid potion unit test

--- src/test/java/org/mockbukkit/mockbukkit/entity/ThrownPotionMockTest.java
+++ src/test/java/org/mockbukkit/mockbukkit/entity/ThrownPotionMockTest.java
@@ -28,6 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class ThrownPotionMockTest
 {
@@ -94,9 +95,6 @@ class ThrownPotionMockTest
 
 	@ParameterizedTest
 	@MethodSource("getThrowablePotionMaterials")
-	@EnumSource(value = Material.class, names = {
-			"DIRT", "DIAMOND", "ACACIA_BOAT"
-	})
 	void getPotionMeta_GivenValidPotion(Material input)
 	{
 		ItemStack expected = ItemStack.of(input);
@@ -108,6 +106,26 @@ class ThrownPotionMockTest
 		PotionMeta other = potion.getPotionMeta();
 		assertNotSame(actual, other);
 		assertEquals(actual, other);
+	}
+
+	@ParameterizedTest
+	@EnumSource(value = Material.class, names = {
+			"DIRT", "DIAMOND", "ACACIA_BOAT"
+	})
+	void getPotionMeta_GiveInvalidPotion(Material input)
+	{
+		ItemStack expected = ItemStack.of(input);
+		potion.setItem(expected);
+
+		try
+		{
+			potion.getPotionMeta();
+			fail("Non potion material should not be valid potion");
+		}
+		catch (ClassCastException ignored)
+		{
+			// Expected
+		}
 	}
 
 	@Test
