fe60cd1f9b28cfc888809fe2469d7a9aa0b75c74
ens-gijs <ens.gijs@gmail.com>
2024-11-29 17:50:27 -0500

    Add CalibratedSculkSensorStateMock type

---
+++ src/main/java/org/mockbukkit/mockbukkit/block/state/CalibratedSculkSensorStateMock.java
@@ -0,0 +1,26 @@
+package org.mockbukkit.mockbukkit.block.state;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.CalibratedSculkSensor;
+import org.jetbrains.annotations.NotNull;
+
+public class CalibratedSculkSensorStateMock extends SculkSensorStateMock implements CalibratedSculkSensor
+{
+
+	protected CalibratedSculkSensorStateMock(@NotNull Block block)
+	{
+		super(block);
+	}
+
+	public CalibratedSculkSensorStateMock(@NotNull Material material)
+	{
+		super(material);
+	}
+
+	protected CalibratedSculkSensorStateMock(@NotNull SculkSensorStateMock state)
+	{
+		super(state);
+	}
+
+}
