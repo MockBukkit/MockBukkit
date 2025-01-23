99d1d594fa734ca5b1986bcb889dc0e427a6947c
Thorinwasher <ragnarlol96@gmail.com>
2025-01-23 09:09:13 +0100

    style: Follow guidlines

--- src/test/java/org/mockbukkit/mockbukkit/block/BlockTypeMockTest.java
+++ src/test/java/org/mockbukkit/mockbukkit/block/BlockTypeMockTest.java
@@ -3,12 +3,15 @@ package org.mockbukkit.mockbukkit.block;
 import org.bukkit.block.BlockType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BlockTypeMockTest
 {
+
 	@Test
-	void getTyped(){
+	void getTyped()
+	{
 		assertNotNull(BlockType.ACACIA_BUTTON);
 	}
+
 }
