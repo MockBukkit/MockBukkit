33bc3f87bda65821c963b2e6923c469331120628
Hjalmar <ragnarlol96@gmail.com>
2025-03-18 10:03:04 +0100

    fix(super): Avoid using super keyword

--- src/main/java/org/mockbukkit/mockbukkit/block/data/DecoratedPotDataMock.java
+++ src/main/java/org/mockbukkit/mockbukkit/block/data/DecoratedPotDataMock.java
@@ -14,40 +14,38 @@ public class DecoratedPotDataMock extends BlockDataMock implements DecoratedPot
 
 	/**
 	 * Constructs a new {@link DecoratedPotDataMock} for the provided {@link Material}.
-	 *
-	 * @param material The material this data is for.
 	 */
 	public DecoratedPotDataMock()
 	{
 		super(Material.DECORATED_POT);
-		super.set(BlockDataKey.CRACKED, false);
-		super.set(BlockDataKey.FACING, BlockFace.NORTH);
-		super.set(BlockDataKey.WATERLOGGED, false);
+		set(BlockDataKey.CRACKED, false);
+		set(BlockDataKey.FACING, BlockFace.NORTH);
+		set(BlockDataKey.WATERLOGGED, false);
 	}
 
 	@Override
 	public boolean isCracked()
 	{
-		return super.get(BlockDataKey.CRACKED);
+		return get(BlockDataKey.CRACKED);
 	}
 
 	@Override
 	public void setCracked(boolean cracked)
 	{
-		super.set(BlockDataKey.CRACKED, cracked);
+		set(BlockDataKey.CRACKED, cracked);
 	}
 
 	@Override
 	public @NotNull BlockFace getFacing()
 	{
-		return super.get(BlockDataKey.FACING);
+		return get(BlockDataKey.FACING);
 	}
 
 	@Override
 	public void setFacing(@NotNull BlockFace blockFace)
 	{
 		Preconditions.checkArgument(getFaces().contains(blockFace), "Illegal block face: " + blockFace);
-		super.set(BlockDataKey.FACING, blockFace);
+		set(BlockDataKey.FACING, blockFace);
 	}
 
 	@Override
@@ -59,13 +57,13 @@ public class DecoratedPotDataMock extends BlockDataMock implements DecoratedPot
 	@Override
 	public boolean isWaterlogged()
 	{
-		return super.get(BlockDataKey.WATERLOGGED);
+		return get(BlockDataKey.WATERLOGGED);
 	}
 
 	@Override
 	public void setWaterlogged(boolean waterlogged)
 	{
-		super.set(BlockDataKey.WATERLOGGED, waterlogged);
+		set(BlockDataKey.WATERLOGGED, waterlogged);
 	}
 
 }
