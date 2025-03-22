0828f189943ba2981760213c3f16da8329092273
4everTheOne <5434488+4everTheOne@users.noreply.github.com>
2025-03-22 01:49:36 +0000

    Replaced exception with UnimplementedOperationException

--- src/main/java/org/mockbukkit/mockbukkit/block/state/SkullStateMock.java
+++ src/main/java/org/mockbukkit/mockbukkit/block/state/SkullStateMock.java
@@ -1,11 +1,9 @@
 package org.mockbukkit.mockbukkit.block.state;
 
-import net.kyori.adventure.text.Component;
-import org.mockbukkit.mockbukkit.exception.UnimplementedOperationException;
-import org.mockbukkit.mockbukkit.profile.PlayerProfileMock;
 import com.destroystokyo.paper.MaterialTags;
 import com.destroystokyo.paper.profile.PlayerProfile;
 import com.google.common.base.Preconditions;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -19,6 +17,8 @@ import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.Rotatable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mockbukkit.mockbukkit.exception.UnimplementedOperationException;
+import org.mockbukkit.mockbukkit.profile.PlayerProfileMock;
 
 /**
  * Mock implementation of a {@link Skull}.
@@ -233,13 +233,13 @@ public class SkullStateMock extends TileStateMock implements Skull
 	@Override
 	public @Nullable Component customName()
 	{
-		throw new UnsupportedOperationException();
+		throw new UnimplementedOperationException();
 	}
 
 	@Override
 	public void customName(@Nullable Component component)
 	{
-		throw new UnsupportedOperationException();
+		throw new UnimplementedOperationException();
 	}
 
 	@Override
