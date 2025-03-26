58e805521ed39e23c4b239ba05995a4e642a46eb
Thorinwasher <ragnarlol96@gmail.com>
2025-03-26 20:09:52 +0100

    chore(dependency): Update junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,8 @@ dependencies {
 	api("io.papermc.paper:paper-api:${property("paper.api.full-version")}")
 
 	// Dependencies for Unit Tests
-	implementation("org.junit.jupiter:junit-jupiter:5.11.4")
+	implementation("org.junit.jupiter:junit-jupiter:5.12.1")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	// General utilities for the project
 	implementation("net.kyori:adventure-platform-bungeecord:4.3.4")
