8018f345689265b128414667f0e0e48d20d431b6
Afonso <5434488+4everTheOne@users.noreply.github.com>
2025-04-19 15:09:52 +0100

    Updated potions to support Splash and Lingering. (#1345)
    * Updated code to use SplashPotion and LingeringPotion
    
    * Readded files removed by commit Added base resources for 1.21.5 (#1341)

---
+++ src/test/java/org/mockbukkit/mockbukkit/entity/SplashPotionMockTest.java
@@ -0,0 +1,34 @@
+package org.mockbukkit.mockbukkit.entity;
+
+import org.bukkit.entity.EntityType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockbukkit.mockbukkit.MockBukkitExtension;
+import org.mockbukkit.mockbukkit.MockBukkitInject;
+import org.mockbukkit.mockbukkit.ServerMock;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockBukkitExtension.class)
+class SplashPotionMockTest
+{
+	@MockBukkitInject
+	private ServerMock server;
+	private SplashPotionMock splashPotion;
+
+	@BeforeEach
+	void setup()
+	{
+		this.splashPotion = new SplashPotionMock(server, UUID.randomUUID());
+	}
+
+	@Test
+	void getType()
+	{
+		assertEquals(EntityType.SPLASH_POTION, splashPotion.getType());
+	}
+
+}
