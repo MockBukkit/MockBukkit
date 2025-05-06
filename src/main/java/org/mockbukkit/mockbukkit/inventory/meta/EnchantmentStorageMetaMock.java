c047592a32ffea01dc85d41c32c8db381e89825a
Thorinwasher <ragnarlol96@gmail.com>
2025-05-06 21:13:44 +0200

    fix(enchantment-storage-meta): Integrate data components

--- src/main/java/org/mockbukkit/mockbukkit/inventory/meta/EnchantmentStorageMetaMock.java
+++ src/main/java/org/mockbukkit/mockbukkit/inventory/meta/EnchantmentStorageMetaMock.java
@@ -1,11 +1,12 @@
 package org.mockbukkit.mockbukkit.inventory.meta;
 
-import com.google.common.collect.ImmutableMap;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Registry;
+import io.papermc.paper.datacomponent.DataComponentType;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemEnchantments;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -19,14 +20,18 @@ import java.util.Map;
 public class EnchantmentStorageMetaMock extends ItemMetaMock implements EnchantmentStorageMeta
 {
 
-	private @NotNull Map<Enchantment, Integer> storedEnchantments = new HashMap<>();
-
 	/**
 	 * Constructs a new {@link EnchantmentStorageMetaMock}.
 	 */
 	public EnchantmentStorageMetaMock()
 	{
 		super();
+	}
+
+	@ApiStatus.Internal
+	public EnchantmentStorageMetaMock(Map<DataComponentType, Object> data)
+	{
+		super(data);
 	}
 
 	/**
@@ -37,45 +42,12 @@ public class EnchantmentStorageMetaMock extends ItemMetaMock implements Enchantm
 	public EnchantmentStorageMetaMock(@NotNull ItemMeta meta)
 	{
 		super(meta);
-
-		if (meta instanceof EnchantmentStorageMeta enchantMeta)
-		{
-			this.storedEnchantments = new HashMap<>(enchantMeta.getStoredEnchants());
-		}
-	}
-
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = super.hashCode();
-		return prime * result + storedEnchantments.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-		{
-			return true;
-		}
-		if (!super.equals(obj))
-		{
-			return false;
-		}
-		if (!(obj instanceof EnchantmentStorageMetaMock other))
-		{
-			return false;
-		}
-		return storedEnchantments.equals(other.storedEnchantments);
 	}
 
 	@Override
 	public @NotNull EnchantmentStorageMetaMock clone()
 	{
-		EnchantmentStorageMetaMock mock = (EnchantmentStorageMetaMock) super.clone();
-		mock.storedEnchantments = new HashMap<>(storedEnchantments);
-		return mock;
+		return (EnchantmentStorageMetaMock) super.clone();
 	}
 
 	@Override
@@ -90,29 +62,49 @@ public class EnchantmentStorageMetaMock extends ItemMetaMock implements Enchantm
 		{
 			return false;
 		}
-
-		Integer prev = storedEnchantments.put(ench, level);
-		return prev == null || prev.intValue() != level;
+		ItemEnchantments itemEnchantments = get(DataComponentTypes.STORED_ENCHANTMENTS);
+		if (itemEnchantments == null)
+		{
+			set(DataComponentTypes.STORED_ENCHANTMENTS, ItemEnchantments.itemEnchantments(Map.of(ench, level)));
+			return true;
+		}
+		Map<Enchantment, Integer> enchantments = new HashMap<>(itemEnchantments.enchantments());
+		if (enchantments.containsKey(ench) && enchantments.get(ench) == level)
+		{
+			return false;
+		}
+		enchantments.put(ench, level);
+		set(DataComponentTypes.STORED_ENCHANTMENTS, ItemEnchantments.itemEnchantments(enchantments));
+		return true;
 	}
 
 	@Override
 	public int getStoredEnchantLevel(@NotNull Enchantment ench)
 	{
-		return storedEnchantments.getOrDefault(ench, 0);
+		ItemEnchantments itemEnchantments = get(DataComponentTypes.STORED_ENCHANTMENTS);
+		if (itemEnchantments == null)
+		{
+			return 0;
+		}
+		return itemEnchantments.enchantments().getOrDefault(ench, 0);
 	}
 
 	@Override
 	public @NotNull Map<Enchantment, Integer> getStoredEnchants()
 	{
-		// This is inline with CraftBukkit's default implementation, it only returns an
-		// immutable copy, never the original.
-		return ImmutableMap.copyOf(storedEnchantments);
+
+		ItemEnchantments itemEnchantments = get(DataComponentTypes.STORED_ENCHANTMENTS);
+		if (itemEnchantments == null)
+		{
+			return Map.of();
+		}
+		return itemEnchantments.enchantments();
 	}
 
 	@Override
 	public boolean hasConflictingStoredEnchant(@NotNull Enchantment ench)
 	{
-		for (Enchantment enchantment : storedEnchantments.keySet())
+		for (Enchantment enchantment : getStoredEnchants().keySet())
 		{
 			if (enchantment.conflictsWith(ench))
 			{
@@ -126,19 +118,27 @@ public class EnchantmentStorageMetaMock extends ItemMetaMock implements Enchantm
 	@Override
 	public boolean hasStoredEnchant(@NotNull Enchantment ench)
 	{
-		return storedEnchantments.containsKey(ench);
+		return getStoredEnchants().containsKey(ench);
 	}
 
 	@Override
 	public boolean hasStoredEnchants()
 	{
-		return !storedEnchantments.isEmpty();
+		return !getStoredEnchants().isEmpty();
 	}
 
 	@Override
 	public boolean removeStoredEnchant(@NotNull Enchantment ench) throws IllegalArgumentException
 	{
-		return storedEnchantments.remove(ench) != null;
+		ItemEnchantments itemEnchantments = get(DataComponentTypes.STORED_ENCHANTMENTS);
+		if (itemEnchantments == null)
+		{
+			return false;
+		}
+		Map<Enchantment, Integer> enchantments = new HashMap<>(itemEnchantments.enchantments());
+		boolean removed = enchantments.remove(enchantments) != null;
+		set(DataComponentTypes.STORED_ENCHANTMENTS, ItemEnchantments.itemEnchantments(enchantments));
+		return removed;
 	}
 
 	/**
@@ -151,39 +151,7 @@ public class EnchantmentStorageMetaMock extends ItemMetaMock implements Enchantm
 	{
 		EnchantmentStorageMetaMock serialMock = new EnchantmentStorageMetaMock();
 		serialMock.deserializeInternal(args);
-		if (args.containsKey("stored-enchantments"))
-		{
-			//noinspection unchecked
-			serialMock.storedEnchantments = ((Map<String, Integer>) args.get("stored-enchantments")).entrySet().stream()
-					.collect(ImmutableMap.toImmutableMap(entry -> getEnchantment(entry.getKey()), Map.Entry::getValue));
-		}
 		return serialMock;
-	}
-
-	/**
-	 * Serializes the properties of an EnchantmentStorageMetaMock to a HashMap.
-	 * Unimplemented properties are not present in the map.
-	 *
-	 * @return A HashMap of String, Object pairs representing the EnchantmentStorageMetaMock.
-	 */
-	@Override
-	public @NotNull Map<String, Object> serialize()
-	{
-		final Map<String, Object> serialized = super.serialize();
-		serialized.put("stored-enchantments", this.storedEnchantments.entrySet().stream()
-				.collect(ImmutableMap.toImmutableMap(entry -> getEnchantmentKey(entry.getKey()), Map.Entry::getValue)));
-		return serialized;
-	}
-
-	private static String getEnchantmentKey(Enchantment enchantment)
-	{
-		return enchantment.getKey().getKey();
-	}
-
-	private static Enchantment getEnchantment(String key)
-	{
-		NamespacedKey namespacedKey = NamespacedKey.minecraft(key);
-		return Registry.ENCHANTMENT.get(namespacedKey);
 	}
 
 	@Override
