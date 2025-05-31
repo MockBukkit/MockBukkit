ebd51eed14f48109dca8945204d8809c635ed5b7
4everTheOne <5434488+4everTheOne@users.noreply.github.com>
2025-05-31 11:33:01 +0100

    Implemented new methods in MusicInstrumentMock

--- src/main/java/org/mockbukkit/mockbukkit/sound/MusicInstrumentMock.java
+++ src/main/java/org/mockbukkit/mockbukkit/sound/MusicInstrumentMock.java
@@ -2,8 +2,11 @@ package org.mockbukkit.mockbukkit.sound;
 
 import com.google.common.base.Preconditions;
 import com.google.gson.JsonObject;
+import net.kyori.adventure.text.Component;
 import org.bukkit.MusicInstrument;
 import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.Sound;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,25 +15,54 @@ public class MusicInstrumentMock extends MusicInstrument
 
 	private final NamespacedKey key;
 	private final String translationKey;
+	private final float duration;
+	private final float range;
+	private final Component description;
+	private final Sound sound;
 
 	/**
-	 * @param key The namespaced key representing this music instrument
+	 * @param key         	The namespaced key representing this music instrument
+	 * @param duration		The music duration.
+	 * @param range			The music range.
+	 * @param description	The music description.
+	 * @param sound			The sound id.
 	 */
-	MusicInstrumentMock(NamespacedKey key, String translationKey)
+	MusicInstrumentMock(@NotNull NamespacedKey key, @NotNull String translationKey, float duration, float range,
+								@NotNull Component description, @NotNull Sound sound)
 	{
-		this.key = key;
-		this.translationKey = translationKey;
+		this.key = Preconditions.checkNotNull(key, "key cannot be null");
+		this.translationKey = Preconditions.checkNotNull(translationKey, "translationKey cannot be null");
+		this.duration = duration;
+		this.range = range;
+		this.description = Preconditions.checkNotNull(description, "description cannot be null");
+		this.sound = Preconditions.checkNotNull(sound, "sound cannot be null");
+
+		Preconditions.checkArgument(Float.isFinite(this.duration) && this.duration >= 0, "duration must be >= 0");
+		Preconditions.checkArgument(Float.isFinite(this.duration) && this.range >= 0, "range must be >= 0");
 	}
 
-	/**
-	 * @param data Json data
-	 * @deprecated Use {@link MusicInstrumentMock(NamespacedKey)} instead
-	 */
-	@Deprecated(forRemoval = true)
-	MusicInstrumentMock(JsonObject data)
+	@Override
+	public float getDuration()
 	{
-		this.key = NamespacedKey.fromString(data.get("key").getAsString());
-		this.translationKey = data.get("translationKey").getAsString();
+		return this.duration;
+	}
+
+	@Override
+	public float getRange()
+	{
+		return this.range;
+	}
+
+	@Override
+	public @NotNull Component description()
+	{
+		return this.description;
+	}
+
+	@Override
+	public @NotNull Sound getSound()
+	{
+		return this.sound;
 	}
 
 	@Override
@@ -53,9 +85,24 @@ public class MusicInstrumentMock extends MusicInstrument
 		Preconditions.checkNotNull(data);
 		Preconditions.checkArgument(data.has("key"), "Missing json key");
 		NamespacedKey key = NamespacedKey.fromString(data.get("key").getAsString());
+		Preconditions.checkNotNull(key, "Invalid namespaced key: " + data.get("key").getAsString());
+
 		Preconditions.checkArgument(data.has("translationKey"), "Missing json translationKey");
 		String translationKey = data.get("translationKey").getAsString();
-		return new MusicInstrumentMock(key, translationKey);
+
+		Preconditions.checkArgument(data.has("duration"), "Missing json duration");
+		float duration = data.get("duration").getAsFloat();
+
+		Preconditions.checkArgument(data.has("range"), "Missing json range");
+		float range = data.get("range").getAsFloat();
+
+		Preconditions.checkArgument(data.has("sound"), "Missing json sound");
+		NamespacedKey soundKey = NamespacedKey.fromString(data.get("sound").getAsString());
+		Preconditions.checkNotNull(soundKey, "Unknown sound with key: " + data.get("sound").getAsString());
+		Sound sound = Registry.SOUNDS.get(soundKey);
+		Preconditions.checkNotNull(sound, "Unknown sound with key: " + soundKey.key().asString());
+
+		return new MusicInstrumentMock(key, translationKey, duration, range, Component.translatable(translationKey), sound);
 	}
 
 }
--- src/test/java/org/mockbukkit/mockbukkit/sound/MusicInstrumentMockTest.java
+++ src/test/java/org/mockbukkit/mockbukkit/sound/MusicInstrumentMockTest.java
@@ -1,6 +1,7 @@
 package org.mockbukkit.mockbukkit.sound;
 
 import com.google.gson.JsonObject;
+import net.kyori.adventure.text.Component;
 import org.bukkit.NamespacedKey;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,7 +22,9 @@ class MusicInstrumentMockTest
 	void setUp()
 	{
 		this.key = new NamespacedKey("mock_bukkit", "custom_music_instrument");
-		this.musicInstrument = new MusicInstrumentMock(key, "instrument.custom.custom_music_instrument");
+		this.musicInstrument = new MusicInstrumentMock(key, "instrument.custom.custom_music_instrument",
+				7, 256, Component.translatable("instrument.custom.custom_music_instrument"),
+				SoundMock.ENTITY_PLAYER_BIG_FALL);
 	}
 
 	@Test
@@ -34,6 +37,31 @@ class MusicInstrumentMockTest
 	void translationKey()
 	{
 		assertEquals("instrument.custom.custom_music_instrument", musicInstrument.translationKey());
+	}
+
+	@Test
+	void getDuration()
+	{
+		assertEquals(7, musicInstrument.getDuration());
+	}
+
+	@Test
+	void getRange()
+	{
+		assertEquals(256, musicInstrument.getRange());
+	}
+
+	@Test
+	void getDescription()
+	{
+		Component description = Component.translatable("instrument.custom.custom_music_instrument");
+		assertEquals(description, musicInstrument.description());
+	}
+
+	@Test
+	void getSound()
+	{
+		assertEquals(SoundMock.ENTITY_PLAYER_BIG_FALL, musicInstrument.getSound());
 	}
 
 	@Test
