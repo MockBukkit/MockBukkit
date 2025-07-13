d39e7e68483e2e7d2d7e74deb492252b4890b250
Steven Van Ingelgem <steven@vaningelgem.be>
2025-07-13 21:19:25 +0200

    revert the removal of comments

--- src/main/java/org/mockbukkit/mockbukkit/potion/ActivePotionEffect.java
+++ src/main/java/org/mockbukkit/mockbukkit/potion/ActivePotionEffect.java
@@ -4,23 +4,44 @@ import org.bukkit.Bukkit;
 import org.bukkit.potion.PotionEffect;
 import org.jetbrains.annotations.NotNull;
 
-public final class ActivePotionEffect implements Comparable<ActivePotionEffect>
+/**
+ * This class represents an active {@link PotionEffect} which was applied to a {@link LivingEntity}.
+ *
+ * @author TheBusyBiscuit
+ * @see LivingEntityMock#addPotionEffect(PotionEffect)
+ */
+public final class ActivePotionEffect
 {
 
 	private final @NotNull PotionEffect effect;
 	private final int startTick;
 
+	/**
+	 * Constructs a new {@link ActivePotionEffect} with the provided {@link PotionEffect}.
+	 *
+	 * @param effect The effect that's been applied.
+	 */
 	public ActivePotionEffect(@NotNull PotionEffect effect)
 	{
 		this.effect = effect;
 		this.startTick = Bukkit.getCurrentTick();
 	}
 
+	/**
+	 * This returns whether this {@link PotionEffect} has expired.
+	 *
+	 * @return Whether the effect wore off.
+	 */
 	public boolean hasExpired()
 	{
 		return getDuration() == 0;
 	}
 
+	/**
+	 * This method returns the underlying {@link PotionEffect}
+	 *
+	 * @return The actual {@link PotionEffect}.
+	 */
 	@NotNull
 	public PotionEffect getPotionEffect()
 	{
@@ -33,6 +54,7 @@ public final class ActivePotionEffect implements Comparable<ActivePotionEffect>
 		{
 			return -1;
 		}
+
 		return Math.max(0, effect.getDuration() - Bukkit.getCurrentTick() + startTick);
 	}
 
