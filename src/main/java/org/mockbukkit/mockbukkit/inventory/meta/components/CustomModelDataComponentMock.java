7813b73966eef8a9e565c14076d1c73d560d1eef
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-07-29 11:45:50 -0400

    added experimental tag

--- src/main/java/org/mockbukkit/mockbukkit/inventory/meta/components/CustomModelDataComponentMock.java
+++ src/main/java/org/mockbukkit/mockbukkit/inventory/meta/components/CustomModelDataComponentMock.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@ApiStatus.Experimental
 public class CustomModelDataComponentMock implements CustomModelDataComponent
 {
 
