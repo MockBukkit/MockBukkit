5ea93880cc02f0dc65145adc8a9186d028edb112
fren_gor <goro@frengor.com>
2025-12-23 09:34:50 +0100

    Do not run cancelListeners if task is already cancelled

--- src/main/java/org/mockbukkit/mockbukkit/scheduler/ScheduledTask.java
+++ src/main/java/org/mockbukkit/mockbukkit/scheduler/ScheduledTask.java
@@ -11,6 +11,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 /**
@@ -22,7 +23,7 @@ public class ScheduledTask implements BukkitTask, BukkitWorker
 	private final int id;
 	private final Plugin plugin;
 	private final boolean isSync;
-	private volatile boolean isCancelled = false;
+	private final AtomicBoolean isCancelled = new AtomicBoolean(false);
 	private volatile long scheduledTick;
 	private volatile boolean running;
 	private final @Nullable Runnable runnable;
@@ -204,14 +205,17 @@ public class ScheduledTask implements BukkitTask, BukkitWorker
 	@Override
 	public boolean isCancelled()
 	{
-		return this.isCancelled;
+		return this.isCancelled.get();
 	}
 
 	@Override
 	public void cancel()
 	{
-		this.isCancelled = true;
-		this.cancelListeners.forEach(Runnable::run);
+		boolean wasCancelled = this.isCancelled.getAndSet(true);
+		if (!wasCancelled)
+		{
+			this.cancelListeners.forEach(Runnable::run);
+		}
 	}
 
 	/**
