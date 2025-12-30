9a8018d4cd1b12f4fb5c43d4ba506d843c1cb24e
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-12-30 21:21:30 +0100

    I forgot an annotation

--- src/main/java/io/papermc/paper/datacomponent/item/ItemComponentTypesBridgeMock.java
+++ src/main/java/io/papermc/paper/datacomponent/item/ItemComponentTypesBridgeMock.java
@@ -15,6 +15,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ItemType;
 import org.bukkit.inventory.meta.trim.ArmorTrim;
 import org.bukkit.map.MapCursor;
+import org.checkerframework.checker.index.qual.NonNegative;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
@@ -332,7 +333,7 @@ public class ItemComponentTypesBridgeMock implements ItemComponentTypesBridge
 	}
 
 	@Override
-	public KineticWeapon.Condition kineticWeaponCondition(int maxDurationTicks, float minSpeed, float minRelativeSpeed)
+	public KineticWeapon.Condition kineticWeaponCondition(@NonNegative int maxDurationTicks, float minSpeed, float minRelativeSpeed)
 	{
 		Preconditions.checkArgument(maxDurationTicks >= 0, "maxDurationTicks must be non-negative");
 		return new KineticWeaponMock.ConditionMock(maxDurationTicks, minSpeed, minRelativeSpeed);
