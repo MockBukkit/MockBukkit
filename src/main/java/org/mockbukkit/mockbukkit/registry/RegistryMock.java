e7326d25365520f331b4fc4a4dd7b2f06765d3ed
westkevin12 <lvvlwest@gmail.com>
2026-04-23 19:09:34 -0500

    refactor: simplify pluralization logic by removing hardcoded switch statements

--- src/main/java/org/mockbukkit/mockbukkit/registry/RegistryMock.java
+++ src/main/java/org/mockbukkit/mockbukkit/registry/RegistryMock.java
@@ -352,51 +352,7 @@ public class RegistryMock<T extends Keyed> implements Registry<T>
 		{
 			return null;
 		}
-		return switch (key.key().value())
-		{
-			case "attribute" -> "attributes";
-			case "banner_pattern" -> "banner_patterns";
-			case "biome" -> "biomes";
-			case "block" -> "blocks";
-			case "cat_variant" -> "cat_variants";
-			case "cat_sound_variant" -> "cat_sound_variants";
-			case "chicken_variant" -> "chicken_variants";
-			case "chicken_sound_variant" -> "chicken_sound_variants";
-			case "cow_variant" -> "cow_variants";
-			case "cow_sound_variant" -> "cow_sound_variants";
-			case "damage_type" -> "damage_types";
-			case "data_component_type" -> "data_component_types";
-			case "dialog" -> "dialogs";
-			case "enchantment" -> "enchantments";
-			case "entity_type" -> "entity_types";
-			case "fluid" -> "fluids";
-			case "frog_variant" -> "frog_variants";
-			case "game_event" -> "game_events";
-			case "game_rule" -> "game_rules";
-			case "instrument" -> "instruments";
-			case "item" -> "items";
-			case "jukebox_song" -> "jukebox_songs";
-			case "map_decoration_type" -> "map_decoration_types";
-			case "memory_module_type" -> "memory_module_types";
-			case "menu" -> "menus";
-			case "mob_effect" -> "mob_effects";
-			case "painting_variant" -> "painting_variants";
-			case "particle_type" -> "particle_types";
-			case "pig_variant" -> "pig_variants";
-			case "pig_sound_variant" -> "pig_sound_variants";
-			case "potion" -> "potions";
-			case "sound_event" -> "sound_events";
-			case "structure" -> "structures";
-			case "structure_type" -> "structure_types";
-			case "trim_material" -> "trim_materials";
-			case "trim_pattern" -> "trim_patterns";
-			case "villager_profession" -> "villager_professions";
-			case "villager_type" -> "villager_types";
-			case "wolf_variant" -> "wolf_variants";
-			case "wolf_sound_variant" -> "wolf_sound_variants";
-			case "zombie_nautilus_variant" -> "zombie_nautilus_variants";
-			default -> key.key().value() + "s";
-		};
+		return key.key().value() + "s";
 	}
 
 }
