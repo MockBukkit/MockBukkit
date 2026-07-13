c3a8b3e31dd2b12fdc88765da730ed7a2a0cc29b
4everTheOne <5434488+4everTheOne@users.noreply.github.com>
2026-07-13 12:47:59 +0100

    Cleanup deprecated methods

--- src/main/java/org/mockbukkit/mockbukkit/util/UnsafeValuesMock.java
+++ src/main/java/org/mockbukkit/mockbukkit/util/UnsafeValuesMock.java
@@ -1,67 +1,40 @@
 package org.mockbukkit.mockbukkit.util;
 
-import com.destroystokyo.paper.util.VersionFetcher;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Multimap;
 import com.google.gson.JsonObject;
 import io.papermc.paper.entity.EntitySerializationFlag;
-import io.papermc.paper.inventory.tooltip.TooltipContext;
-import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
 import net.kyori.adventure.key.Keyed;
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.HoverEvent;
-import net.kyori.adventure.text.flattener.ComponentFlattener;
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import net.kyori.adventure.text.serializer.plain.PlainComponentSerializer;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.kyori.adventure.translation.Translatable;
-import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.RegionAccessor;
 import org.bukkit.Registry;
-import org.bukkit.Statistic;
 import org.bukkit.Tag;
 import org.bukkit.UnsafeValues;
 import org.bukkit.World;
 import org.bukkit.advancement.Advancement;
-import org.bukkit.attribute.Attributable;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.block.data.BlockData;
-import org.bukkit.command.CommandSender;
-import org.bukkit.damage.DamageSource;
-import org.bukkit.damage.DamageType;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.CreativeCategory;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.InvalidPluginException;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionType;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mockbukkit.mockbukkit.damage.DamageSourceBuilderMock;
 import org.mockbukkit.mockbukkit.exception.ItemSerializationException;
 import org.mockbukkit.mockbukkit.exception.UnimplementedOperationException;
 import org.mockbukkit.mockbukkit.inventory.ItemStackMock;
 import org.mockbukkit.mockbukkit.inventory.SerializableMeta;
 import org.mockbukkit.mockbukkit.inventory.meta.ItemMetaMock;
 import org.mockbukkit.mockbukkit.inventory.serializer.SerializationUtils;
-import org.mockbukkit.mockbukkit.plugin.lifecycle.event.LifecycleEventManagerMock;
 import org.mockbukkit.mockbukkit.potion.InternalPotionDataMock;
 
 import java.io.ByteArrayInputStream;
@@ -73,9 +46,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
-import java.util.function.BooleanSupplier;
 
 /**
  * Mock implementation of an {@link UnsafeValues}.
@@ -117,54 +88,6 @@ public class UnsafeValuesMock implements UnsafeValues
 	}
 
 	private String minimumApiVersion = "none";
-
-	@Override
-	public @NotNull ComponentFlattener componentFlattener()
-	{
-		return ComponentFlattener.basic();
-	}
-
-	@Override
-	@Deprecated(forRemoval = true, since = "1.18")
-	public @NotNull PlainComponentSerializer plainComponentSerializer()
-	{
-		return PlainComponentSerializer.plain();
-	}
-
-	@Override
-	@Deprecated(forRemoval = true, since = "1.18")
-	public @NotNull PlainTextComponentSerializer plainTextSerializer()
-	{
-		return PlainTextComponentSerializer.plainText();
-	}
-
-	@Override
-	@Deprecated(forRemoval = true, since = "1.18")
-	public @NotNull GsonComponentSerializer gsonComponentSerializer()
-	{
-		return GsonComponentSerializer.gson();
-	}
-
-	@Override
-	@Deprecated(forRemoval = true, since = "1.18")
-	public @NotNull GsonComponentSerializer colorDownsamplingGsonComponentSerializer()
-	{
-		return GsonComponentSerializer.colorDownsamplingGson();
-	}
-
-	@Override
-	@Deprecated(forRemoval = true, since = "1.18")
-	public @NotNull LegacyComponentSerializer legacyComponentSerializer()
-	{
-		return LegacyComponentSerializer.legacySection();
-	}
-
-	@Override
-	public Component resolveWithContext(Component component, CommandSender context, Entity scoreboardSubject, boolean bypassPermissions)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
 
 	@Override
 	public Material toLegacy(Material material)
@@ -283,34 +206,12 @@ public class UnsafeValuesMock implements UnsafeValues
 	}
 
 	@Override
-	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(Material material, EquipmentSlot slot)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public CreativeCategory getCreativeCategory(Material material)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public VersionFetcher getVersionFetcher()
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
 	@Deprecated(forRemoval = true, since = "1.19")
 	public boolean isSupportedApiVersion(String apiVersion)
 	{
 		return COMPATIBLE_API_VERSIONS.contains(apiVersion);
 	}
 
-	@Override
 	public byte[] serializeItem(ItemStack item)
 	{
 		Preconditions.checkNotNull(item, "null cannot be serialized");
@@ -320,7 +221,7 @@ public class UnsafeValuesMock implements UnsafeValues
 		final ByteArrayOutputStream bao = new ByteArrayOutputStream();
 		try
 		{
-			@NotNull Map<String, Object> stack = this.serializeStack(item);
+			@NotNull Map<String, Object> stack = serializeStack(item);
 			final ObjectOutputStream oos = new BukkitObjectOutputStream(bao);
 			oos.writeObject(stack);
 			return bao.toByteArray();
@@ -331,7 +232,6 @@ public class UnsafeValuesMock implements UnsafeValues
 		}
 	}
 
-	@Override
 	public ItemStack deserializeItem(byte[] data)
 	{
 		Preconditions.checkNotNull(data, "null cannot be deserialized");
@@ -399,92 +299,6 @@ public class UnsafeValuesMock implements UnsafeValues
 		throw new UnimplementedOperationException();
 	}
 
-	@Override
-	@Nullable
-	@Deprecated(since = "1.21", forRemoval = true)
-	public String getBlockTranslationKey(@NotNull Material material)
-	{
-		if (!material.isBlock())
-		{
-			return null;
-		}
-		// edge cases: WHEAT and NETHER_WART are blocks, but still use the "item" prefix
-		if (material == Material.WHEAT || material == Material.NETHER_WART)
-		{
-			return formatTranslatable("item", material);
-		}
-		return formatTranslatable("block", material);
-	}
-
-	@Override
-	@Nullable
-	@Deprecated(since = "1.21", forRemoval = true)
-	public String getItemTranslationKey(@NotNull Material material)
-	{
-		if (!material.isItem())
-		{
-			return null;
-		}
-		String edgeCaseHandledTranslationKey = handleTranslateItemEdgeCases(material);
-		if (edgeCaseHandledTranslationKey != null)
-		{
-			return edgeCaseHandledTranslationKey;
-		}
-		return formatTranslatable("item", material);
-	}
-
-	@Override
-	@Nullable
-	public String getTranslationKey(@NotNull EntityType type)
-	{
-		Preconditions.checkArgument(type.getName() != null, "Invalid name of EntityType %s for translation key", type);
-		return formatTranslatable("entity", type);
-	}
-
-	@Override
-	@Nullable
-	public String getTranslationKey(@NotNull ItemStack itemStack)
-	{
-		if (itemStack.getType().isItem())
-		{
-			Material material = itemStack.getType();
-			String edgeCaseHandledTranslationKey = handleTranslateItemEdgeCases(material);
-			return Objects.requireNonNullElseGet(edgeCaseHandledTranslationKey, () -> formatTranslatable("item", material, true));
-		}
-		else if (itemStack.getType().isBlock())
-		{
-			return getBlockTranslationKey(itemStack.getType());
-		}
-		else
-		{
-			return null;
-		}
-	}
-
-	@Override
-	public String getTranslationKey(Attribute attribute)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	private String handleTranslateItemEdgeCases(Material material)
-	{
-		// edge cases: WHEAT and NETHER_WART are blocks, but still use the "item" prefix (therefore this check has to be done BEFORE the isBlock check below)
-		if (material == Material.WHEAT || material == Material.NETHER_WART)
-		{
-			return formatTranslatable("item", material);
-		}
-		// edge case: If a translation key from an item is requested from anything that is also a block, the block translation key is always returned
-		// e.g: Material#STONE is a block (but also an obtainable item in the inventory). However, the translation key is always "block.minecraft.stone".
-		if (material.isBlock())
-		{
-			return formatTranslatable("block", material);
-		}
-		// not an edge case
-		return null;
-	}
-
 	private <T extends Keyed & Translatable> String formatTranslatable(String prefix, T translatable, boolean fromItemStack)
 	{
 		// enforcing Translatable is not necessary, but translating only makes sense when the object is really translatable by design.
@@ -511,15 +325,11 @@ public class UnsafeValuesMock implements UnsafeValues
 	}
 
 	@Override
+	@ApiStatus.Internal
+	@Deprecated(since = "1.20.2", forRemoval = true)
 	public PotionType.InternalPotionData getInternalPotionData(NamespacedKey key)
 	{
 		return new InternalPotionDataMock(key);
-	}
-
-	@Override
-	public DamageSource.@NotNull Builder createDamageSourceBuilder(@NotNull DamageType damageType)
-	{
-		return new DamageSourceBuilderMock(damageType);
 	}
 
 	@Override
@@ -537,7 +347,7 @@ public class UnsafeValuesMock implements UnsafeValues
 	}
 
 	@Override
-	public int nextEntityId()
+	public int nextEntityId(World world)
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
@@ -550,84 +360,12 @@ public class UnsafeValuesMock implements UnsafeValues
 	}
 
 	@Override
-	public boolean isValidRepairItemStack(@NotNull ItemStack itemToBeRepaired, @NotNull ItemStack repairMaterial)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
 	public int getProtocolVersion()
 	{
 		// TODO Auto-generated method stub
 		throw new UnimplementedOperationException();
 	}
 
-	@Override
-	public boolean hasDefaultEntityAttributes(@NotNull NamespacedKey entityKey)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public @NotNull Attributable getDefaultEntityAttributes(@NotNull NamespacedKey entityKey)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public @NotNull NamespacedKey getBiomeKey(RegionAccessor accessor, int x, int y, int z)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public void setBiomeKey(RegionAccessor accessor, int x, int y, int z, NamespacedKey biomeKey)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public String getStatisticCriteriaKey(@NotNull Statistic statistic)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public @Nullable Color getSpawnEggLayerColor(EntityType entityType, int i)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public LifecycleEventManager<Plugin> createPluginLifecycleEventManager(JavaPlugin javaPlugin,
-																		   BooleanSupplier booleanSupplier)
-	{
-		return new LifecycleEventManagerMock<>(javaPlugin, booleanSupplier);
-	}
-
-	@Override
-	public @NotNull List<Component> computeTooltipLines(@NotNull ItemStack itemStack,
-														@NotNull TooltipContext tooltipContext,
-														@Nullable Player player)
-	{
-		// TODO Auto-generated method stub
-		throw new UnimplementedOperationException();
-	}
-
-	@Override
-	public ItemStack createEmptyStack()
-	{
-		return ItemStackMock.empty();
-	}
-
-	@Override
 	public @NotNull Map<String, Object> serializeStack(ItemStack itemStack)
 	{
 		if (itemStack.isEmpty())
