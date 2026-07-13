2b77d1ef593c8b14194f52ce5d53a8b22c383e85
4everTheOne <5434488+4everTheOne@users.noreply.github.com>
2026-07-13 12:47:41 +0100

    Added support for SulfurCubeContentMock

---
+++ src/main/java/io/papermc/paper/datacomponent/item/SulfurCubeContentMock.java
@@ -0,0 +1,16 @@
+package io.papermc.paper.datacomponent.item;
+
+import org.bukkit.inventory.ItemStack;
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+@SuppressWarnings({ "UnstableApiUsage", "NonExtendableApiUsage" })
+public record SulfurCubeContentMock(ItemStack absorbedItem) implements SulfurCubeContent {
+
+	@Override
+	public ItemStack absorbedItem()
+	{
+		return this.absorbedItem;
+	}
+
+}
